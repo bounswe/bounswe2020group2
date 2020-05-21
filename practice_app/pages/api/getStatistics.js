765da605acb9e639805483ab5f9bc908948a7a46
Mehdi Saffar <saffar.mehdi3@gmail.com>
2020-05-21 19:11:32 +0300

    Small refactoring

--- practice_app/pages/api/getStatistics.js
+++ practice_app/pages/api/getStatistics.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const {checking} = require('./getStatisticsModule');
+const { checking } = require('./getStatisticsModule')
 
 export default (req, res) => {
     // req object contains whatever information we can get from the request that comes from the client (from the browser)
@@ -8,34 +8,34 @@ export default (req, res) => {
     const { query, method } = req
 
     switch (method) {
-        case 'GET': {
-            res.statusCode = 200
-            
-            const countryName = query.country
+        case 'GET':
+            {
+                const countryName = query.country
 
-            axios.get("https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true")
+                axios
+                    .get(
+                        'https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true',
+                    )
+                    .then(response => {
+                        const values = checking(response, countryName)
 
-            .then((response) => {
-                
-                const values= checking(response,countryName)
-            
-                let deathToll1 = values.deathToll1
-                let recovered1 = values.recovered1
-                let answered1 = values.answered1
-                let infected1  = values.infected1
-  
-                res.json({
-                    answered: answered1,
-                    deathToll: deathToll1,
-                    recovery: recovered1,
-                    infection:infected1                   
-                })
-              }).catch(function (error) {                
-            res.statusCode = 500
-            res.json({})
-            console.log(error);
-                    });        
-        } break
+                        const { deathToll1, recovered1, answered1, infected1 } = values
+
+                        res.statusCode = 200
+                        res.json({
+                            answered: answered1,
+                            deathToll: deathToll1,
+                            recovery: recovered1,
+                            infection: infected1,
+                        })
+                    })
+                    .catch(error => {
+                        console.log(error)
+                        res.statusCode = 500
+                        res.json({})
+                    })
+            }
+            break
         default: {
             res.statusCode = 200
             res.send('please use GET')
--- practice_app/pages/api/getStatisticsModule.js
+++ practice_app/pages/api/getStatisticsModule.js
@@ -1,33 +1,34 @@
-exports.checking = function(response, countryName) {
-    
-    let isCountryNull = "no"
-    if(countryName == null)
-    {
-        isCountryNull = "yes"
-        return {isCountryNull:isCountryNull}
+exports.checking = function (response, countryName) {
+    let isCountryNull = 'no'
+    if (countryName == null) {
+        isCountryNull = 'yes'
+        return { isCountryNull }
     }
-    if(response == null)
-    {
-        return {isCountryNull:isCountryNull,answered1:"no"}    
+    if (response == null) {
+        return { isCountryNull, answered1: 'no' }
     }
     let i
-    let deathToll2 
+    let deathToll2
     let recovered2
     let answered2
-    let infected2   
+    let infected2
 
-    for (i = 0; i < response.data.length; i++) 
-    {
-        if(response.data[i].country==countryName)
-        {
-            answered2 = "yes"
-            deathToll2= response.data[i].deceased
-            recovered2= response.data[i].recovered
-            infected2= response.data[i].infected
-                        
+    for (i = 0; i < response.data.length; i++) {
+        if (response.data[i].country == countryName) {
+            answered2 = 'yes'
+            deathToll2 = response.data[i].deceased
+            recovered2 = response.data[i].recovered
+            infected2 = response.data[i].infected
+
             break
-        }                   
-    }      
-    
-    return {deathToll1:deathToll2,recovered1:recovered2,answered1:answered2,infected1:infected2,isCountryNull:isCountryNull}
+        }
+    }
+
+    return {
+        deathToll1: deathToll2,
+        recovered1: recovered2,
+        answered1: answered2,
+        infected1: infected2,
+        isCountryNull,
+    }
 }
