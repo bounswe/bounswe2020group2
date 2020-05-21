042278f69f8bba9d6e6d0ad4ea094eed4203fec9
Furkan Nane <32761817+furkannane@users.noreply.github.com>
2020-05-21 18:59:46 +0300

    Update getStatistics.js

--- practice_app/pages/api/getStatistics.js
+++ practice_app/pages/api/getStatistics.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const { checking } = require('./getStatisticsModule')
+const {checking} = require('./getStatisticsModule');
 
 export default (req, res) => {
     // req object contains whatever information we can get from the request that comes from the client (from the browser)
@@ -8,54 +8,34 @@ export default (req, res) => {
     const { query, method } = req
 
     switch (method) {
-        case 'GET':
-            {
-                // ...
-                res.statusCode = 200
+        case 'GET': {
+            res.statusCode = 200
+            
+            const countryName = query.country
 
-                const countryName = query.country
+            axios.get("https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true")
 
-                let deathToll1
-                let recovered1
-                let answered1
-                let infected1
-
-                axios
-                    .get(
-                        'https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true',
-                    )
-
-                    .then(response => {
-                        let i
-
-                        for (i = 0; i < response.data.length; i++) {
-                            if (response.data[i].country == countryName) {
-                                answered1 = 'yes'
-                                deathToll1 = response.data[i].deceased
-                                recovered1 = response.data[i].recovered
-                                infected1 = response.data[i].infected
-
-                                break
-                            }
-                        }
-
-                        const answered2 = checking({ countryN: countryName, answer: answered1 }).ansnull
-
-                        res.statusCode = 200
-                        res.json({
-                            answered: answered2,
-                            deathToll: deathToll1,
-                            recovery: recovered1,
-                            infection: infected1,
-                        })
-                    })
-                    .catch(error => {
-                        res.statusCode = 500
-
-                        console.log(error)
-                    })
-            }
-            break
+            .then((response) => {
+                
+                const values= checking(response,countryName)
+            
+                let deathToll1 = values.deathToll1
+                let recovered1 = values.recovered1
+                let answered1 = values.answered1
+                let infected1  = values.infected1
+  
+                res.json({
+                    answered: answered1,
+                    deathToll: deathToll1,
+                    recovery: recovered1,
+                    infection:infected1                   
+                })
+              }).catch(function (error) {                
+            res.statusCode = 500
+            res.json({})
+            console.log(error);
+                    });        
+        } break
         default: {
             res.statusCode = 200
             res.send('please use GET')
