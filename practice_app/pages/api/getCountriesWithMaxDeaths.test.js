79772ea6db393078e1fb1548ab378a923f65a666
omerfdeniz <omerfaruk.deniz@gmail.com>
2020-05-24 02:44:20 +0300

    Added code for covid-19 top 10 stats

---
+++ practice_app/pages/api/getCountriesWithMaxDeaths.test.js
@@ -0,0 +1,12 @@
+const { getTop10DataFor } = require('./getCountriesWithMaxDeathsModule')
+const response = require('./examplecoviddata.json')
+describe('getTop10DataFor', () => {
+    test('whether response has at least 10 countries', () => {
+        const data = getTop10DataFor(response)
+        expect(data.keys.length).toEqual(10)
+    })
+    test('whether response has at least 10 death numbers', () => {
+        const data = getTop10DataFor(response)
+        expect(data.values.length).toEqual(10)
+    })
+})
