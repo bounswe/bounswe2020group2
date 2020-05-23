8266bd25c8c18154b90219b8abfc61bb042f4565
Asli <56061820+asliayk@users.noreply.github.com>
2020-05-23 23:17:19 +0300

    Indentation&typo error fixed
    Fixed variable name error. Completed indentation.

--- practice_app/pages/api/getAllProducts.js
+++ practice_app/pages/api/getAllProducts.js
@@ -1,47 +1,46 @@
-const getAllProducts = (req,res) => new Promise(
-    async resolve => { 
-   // Imports the Google Cloud client library
-   const vision = require('@google-cloud/vision');
+const getAllProducts = (req, res) =>
+  new Promise(async (resolve) => {
+    // Imports the Google Cloud client library
+    const vision = require("@google-cloud/vision");
 
-   // Creates a client
-   const client = new vision.ProductSearchClient();
- 
-   // Google cloud project id and region name
-   const projectId = 'brilliant-era-276800';
-   const location = 'europe-west1';
+    // Creates a client
+    const client = new vision.ProductSearchClient();
 
-// Resource path that represents Google Cloud Platform location.
-const formattedParent = client.locationPath(projectId, location);
+    // Google cloud project id and region name
+    const projectId = "brilliant-era-276800";
+    const location = "europe-west1";
 
-// Initializes HTML text
-var mytext = '<h1> Products </h1>';  
+    // Resource path that represents Google Cloud Platform location.
+    const formattedParent = client.locationPath(projectId, location);
 
-// Calls product client of Google Product Search API via listProducts
-client.listProducts({parent: formattedParent})
-  .then(responses => {
-    const products = responses[0];
-    // checks whether there is a product
-    if(products.length==0) {
-        response.on("finish", resolve);
-        response.send({"text": "There is no products"})
-        return;
-    }
-    // Adds names of each product to HTML text
-    for (const product of products) {
-      mytext += '<li>' + product.name + '</li>';
-    }
-    
-    // Sends response text to HTML page
-    if(mytext!=='<h1> Products </h1>') {
-    response.on("finish", resolve);
-    response.send('<p>' +mytext +'</p>');
-    }
+    // Initializes HTML text
+    var mytext = "<h1> Products </h1>";
 
+    // Calls product client of Google Product Search API via listProducts
+    client
+      .listProducts({ parent: formattedParent })
+      .then((responses) => {
+        const products = responses[0];
+        // checks whether there is a product
+        if (products.length == 0) {
+          res.on("finish", resolve);
+          res.send({ text: "There is no products" });
+          return;
+        }
+        // Adds names of each product to HTML text
+        for (const product of products) {
+          mytext += "<li>" + product.name + "</li>";
+        }
 
-  })
-  .catch(err => {
-    // Shows error message
-    console.error(err);
+        // Sends response text to HTML page
+        if (mytext !== "<h1> Products </h1>") {
+          res.on("finish", resolve);
+          res.send("<p>" + mytext + "</p>");
+        }
+      })
+      .catch((err) => {
+        res.on("finish", resolve);
+        res.send("<p>There is no product in ");
+      });
   });
-})
 module.exports = getAllProducts;
