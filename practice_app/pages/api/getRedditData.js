ea573c35c0bfd6852cdc7bdb41fea396fd1af84c
Burak Çetin <burak.cetin@boun.edu.tr>
2020-05-24 19:45:54 +0300

    Seperate GET from Module

--- practice_app/pages/api/getRedditData.js
+++ practice_app/pages/api/getRedditData.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const { getPostData } = require('./getRedditDataModule')
 
 export default (req, res) => {
     // req object contains whatever information we can get from the request that comes from the client (from the browser)
@@ -13,17 +14,12 @@ export default (req, res) => {
                 axios
                     .get('http://www.reddit.com/r/' + countryName + '/top/.json?limit=10&t=month')
                     .then(response => {
-                        const values = response.data.data.children
-						var outJSON = []
-						for(let i = 0; i < values.length; i++){
-							var tmp = {title : values[i].data.title,permalink : values[i].data.permalink};
-							outJSON.push(tmp);
-						}
+						var extractedData = getPostData(response)
 
                         res.statusCode = 200
                         res.json({
                             answered: 'yes',
-                            posts:outJSON,
+                            posts:extractedData,
                         })
                     })
                     .catch(error => {
---
+++ practice_app/pages/api/getRedditDataModule.js
@@ -0,0 +1,11 @@
+exports.getPostData = function (response) {
+
+	const values = response.data.data.children
+	var outJSON = []
+	for(let i = 0; i < values.length; i++){
+		var tmp = {title : values[i].data.title,permalink : values[i].data.permalink}
+		outJSON.push(tmp)
+	}
+	return outJSON
+
+}
