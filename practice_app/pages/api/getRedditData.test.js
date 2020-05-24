d8ef63576116cba8c2590feaa5ef5ff11e38e9fb
Burak Çetin <burak.cetin@boun.edu.tr>
2020-05-24 19:58:22 +0300

    Add test data and initialize test code

--- practice_app/pages/api/getRedditData.test.js
+++ practice_app/pages/api/getRedditData.test.js
@@ -1,8 +1,16 @@
-const { checkGetRedditData } = require('./getRedditData')
-
+const { checkGetRedditData } = require('./getRedditDataModule')
+const testResponse = require('./examplecoviddata.json')
 describe('checkGetRedditData', () => {
-    test('check if data is collected', () => {
-        const { data } = axios.get(`api/getRedditData?country=Turkey`)
-        expect(data.answered).toBe('yes')
+    test('check if there are 10 posts', () => {
+        const data = getPostData(testResponse)
+        expect(testResponse.length).toBe(10)
+    })
+	test('check title extraction', () => {
+        const data = getPostData(testResponse)
+        expect(testResponse[0].title).toBe("Hello Ekrem")
+    })
+	test('check permalink extraction', () => {
+        const data = getPostData(testResponse)
+        expect(testResponse[0].permalink).toBe("/r/Turkey/comments/gijpwa/hello_ekrem/")
     })
 })
