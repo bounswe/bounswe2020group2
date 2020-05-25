f7f0ddafaa9e4c1bfcdba50d41905ba2faa8a5f6
Furkan <afrknvrl@gmail.com>
2020-05-25 21:36:18 +0300

    fixes

--- practice_app/pages/api/getRedditData.test.js
+++ practice_app/pages/api/getRedditData.test.js
@@ -4,14 +4,14 @@ const testResponse = require('./exampleRedditData.json')
 describe('checkGetRedditData', () => {
     test('check if there are 10 posts', () => {
         const data = getPostData(testResponse)
-        expect(testResponse.length).toBe(10)
+        expect(data.length).toBe(10)
     })
     test('check title extraction', () => {
         const data = getPostData(testResponse)
-        expect(testResponse[0].title).toBe('Hello Ekrem')
+        expect(data[0].title).toBe('Hello Ekrem')
     })
     test('check permalink extraction', () => {
         const data = getPostData(testResponse)
-        expect(testResponse[0].permalink).toBe('/r/Turkey/comments/gijpwa/hello_ekrem/')
+        expect(data[0].permalink).toBe('/r/Turkey/comments/gijpwa/hello_ekrem/')
     })
 })
