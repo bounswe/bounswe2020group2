8b3583fbadc2fd9fb426302f819beef30a296217
karalleyna <36455180+karalleyna@users.noreply.github.com>
2020-05-27 15:39:18 +0300

    Update read part of image

--- practice_app/pages/api/getSimilarProducts.js
+++ practice_app/pages/api/getSimilarProducts.js
@@ -1,102 +1,97 @@
-const getSimilarProducts = (req, res) =>
-  new Promise(async (resolve) => {
-    // Imports the Google Cloud client library
-    const vision = require("@google-cloud/vision");
-    // Id of the getflix in Google Cloud Storage
-    const projectId = "brilliant-era-276800";
-    // Location of the getflix in Google Cloud Storage
-    const location = "europe-west1";
-    // Imports file system module to read images
-    const fs = require("fs");
-    // Creates a product client
-    const productSearchClient = new vision.ProductSearchClient();
-    // Creates an image client
-    const imageAnnotatorClient = new vision.ImageAnnotatorClient();
-    // Gets url of the current page
-    var requestURL = req.url;
-    // Parses url into query parameters
-    var initial = requestURL.indexOf("?");
-    initial = initial + 1;
-    requestURL = requestURL.substring(initial);
-    requestURL = requestURL.split("&");
-    var queryParams = {};
-    requestURL.forEach((pair) => {
-      var keyValue = pair.split("=");
-      if (keyValue[1].includes("%3D")) {
-        keyValue[1] = keyValue[1].split("%3D").join("=");
-      }
-      if (keyValue[1].includes("%3A")) {
-        keyValue[1] = keyValue[1].split("%3A").join(":");
-      }
-      if (keyValue[1].includes("%2F")) {
-        keyValue[1] = keyValue[1].split("%2F").join("/");
-      }
-      if (keyValue[1].includes("%5C")) {
-        keyValue[1] = keyValue[1].split("%5C").join("\\");
-      }
-      queryParams[keyValue[0]] = keyValue[1];
-    });
-    const productSetId = queryParams["setId"];
-    const productCategory = queryParams["category"];
-    const filePath = queryParams["filePath"];
-    const filter = queryParams["filter"];
-    // Determines the path of the product set via 'productSetPath' function of Google Product Search API
-    const productSetPath = productSearchClient.productSetPath(
-      projectId,
-      location,
-      productSetId
-    );
-    // The image data as base64-encoded text
-    const content = fs.readFileSync(filePath, "base64");
-    // Initializes the request object with the corresponding form.
-    const request = {
-      image: { content: content },
-      features: [{ type: "PRODUCT_SEARCH" }],
-      imageContext: {
-        productSearchParams: {
-          productSet: productSetPath,
-          productCategories: [productCategory],
-          filter: filter,
-        },
-      },
-    };
+// Imports the Google Cloud client library
+const vision = require("@google-cloud/vision");
+// Id of the getflix in Google Cloud Storage
+const projectId = "brilliant-era-276800";
+// Location of the getflix in Google Cloud Storage
+const location = "europe-west1";
+// Imports file system module to read images
+const fs = require("fs");
 
-    // Async function call to image client via it's 'batchAnnotateImages' function
-    const [response] = await imageAnnotatorClient
-      .batchAnnotateImages({
-        requests: [request],
-      })
-      .catch((err) => {
-        res.statusCode = 400;
-        res.statusText = "The size of page is greater than 100 or less than 1.";
-        return res;
-      });
-    var results = response["responses"][0]["productSearchResults"];
+function decodeBase64Image(dataString) {
+  var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+    response = {};
+
+  if (matches.length !== 3) {
+    return new Error("Invalid input string");
+  }
+
+  response.type = matches[1];
+  response.data = new Buffer(matches[2], "base64");
+
+  return response;
+}
+
+module.exports = async function (req, res) {
+  // Creates a product client
+  const productSearchClient = new vision.ProductSearchClient();
+  // Creates an image client
+  const imageAnnotatorClient = new vision.ImageAnnotatorClient();
+
+  const {
+    setId: productSetId,
+    category: productCategory,
+    imageBase64,
+    filter,
+  } = req.body;
+
+  // Determines the path of the product set via 'productSetPath' function of Google Product Search API
+  const productSetPath = productSearchClient.productSetPath(
+    projectId,
+    location,
+    productSetId
+  );
+  // The image data as base64-encoded text
+  const content = decodeBase64Image(imageBase64).data;
+
+  // fs.writeFileSync('./image.jpg', content)
+
+  // const content = fs.readFileSync(filePath, 'base64')
+  // Initializes the request object with the corresponding form.
+  const request = {
+    image: { content: content },
+    features: [{ type: "PRODUCT_SEARCH" }],
+    imageContext: {
+      productSearchParams: {
+        productSet: productSetPath,
+        productCategories: [productCategory],
+        filter: filter,
+      },
+    },
+  };
+
+  // Async function call to image client via it's 'batchAnnotateImages'
+  // function
+  try {
+    const [response] = await imageAnnotatorClient.batchAnnotateImages({
+      requests: [request],
+    });
+
+    let results = response["responses"][0]["productSearchResults"];
     // Checks whether results is empty or not
-    if (!results) {
-      res.statusCode = 404;
-      res.statusText = "Results cannot be found.";
-      res.send("There is no result.");
-      return res;
-    }
+    if (!results)
+      return res.json({
+        statusCode: 404,
+        statusText: "Results cannot be found.",
+      });
+
     // Contains all products similar to image given by end user
     results = results["results"];
-    var temp = "";
-    results.forEach((result) => {
-      temp +=
-        "<h2>Product id:" + result["product"].name.split("/").pop(-1) + "</h2>";
-      temp += "<li>Product name: " + result["product"].displayName + "</li>";
-      temp +=
-        "<li>Product description: " + result["product"].description + "</li>";
-      temp +=
-        "<li>Product category: " + result["product"].productCategory + "</li>";
-      temp += "<li>Product similarity score: " + result.score + "</li>";
+
+    const json = results.map((result) => {
+      return {
+        id: result.product.name.split("/").pop(-1),
+        name: result.product.displayName,
+        description: result.product.description,
+        category: result.product.productCategory,
+        score: result.score,
+      };
     });
-    // Succesful response
-    res.statusCode = 200;
-    res.statusText = "Similar products can be determined.";
-    res.on("finish", resolve);
-    res.send("<p>" + temp + "</p>");
-    return res;
-  });
-module.exports = getSimilarProducts;
+    res.json(json);
+  } catch (err) {
+    console.log(err);
+    res.json({
+      statusCode: 400,
+      statusText: "The size of page is greater than 100 or less than 1.",
+    });
+  }
+};
