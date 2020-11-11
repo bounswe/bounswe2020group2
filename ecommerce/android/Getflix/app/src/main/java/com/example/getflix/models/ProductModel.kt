6ccc21a68663569b7bab6e4afc9d8a9767e0dd1b
hande <hansirikci@gmail.com>
2020-11-11 21:25:47 +0300

    product card created and designed

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/models/ProductModel.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/models/ProductModel.kt
@@ -3,5 +3,8 @@ package com.example.getflix.models
 data class ProductModel(
     var id: Int,
     var name: String,
-    var category: String
+    var category: String,
+    var price: Float,
+    var brandId: String,
+    var description: String
 )
