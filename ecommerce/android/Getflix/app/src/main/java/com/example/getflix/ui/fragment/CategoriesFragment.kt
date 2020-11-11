01ff79ff2123bdb13cafeb0bcd7db168c3201968
karalleyna <36455180+karalleyna@users.noreply.github.com>
2020-11-12 01:41:56 +0300

    Add CategoriesViewModel

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragment/CategoriesFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragment/CategoriesFragment.kt
@@ -6,9 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ExpandableListView
 import androidx.databinding.DataBindingUtil
+
 import androidx.fragment.app.Fragment
+
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.getflix.R
+import com.example.getflix.adapters.SubcategoryAdapter
 import com.example.getflix.databinding.FragmentCategoriesBinding
+
 import com.thoughtbot.expandablerecyclerview.listeners.OnGroupClickListener
 /* import com.example.getflix.model.CategoryModel
 import com.example.getflix.model.SubcategoryModel
@@ -18,13 +24,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.* */
 
 
+
+
+
 class CategoriesFragment : Fragment() {
+    private lateinit var categoriesViewModel: CategoriesViewModel
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         val binding = DataBindingUtil.inflate<FragmentCategoriesBinding>(
             inflater, R.layout.fragment_categories,
             container, false
@@ -50,10 +61,16 @@ class CategoriesFragment : Fragment() {
         binding.catRec.adapter = adapter */
 
 
+        categoriesViewModel =  ViewModelProvider(this).get(CategoriesViewModel::class.java)
+        val adapter = SubcategoryAdapter()
+        categoriesViewModel.displayedCategories.observe(viewLifecycleOwner, Observer {
+            it.let {
+                adapter.submitList(it["woman"])
+            }
+        })
         return binding.root
     }
 
 
-
-
 }
+
