f93471f6e45b26acaee37bf54a433f23b33fc5a0
Mehdi Saffar <saffar.mehdi3@gmail.com>
2020-11-12 21:49:58 +0300

    Add initial routing, add initial layout

--- ecommerce/frontend/src/App.js
+++ ecommerce/frontend/src/App.js
@@ -1,20 +1,71 @@
 import logo from './logo.svg'
 import './App.less'
 import config from './config.js'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { Layout, Menu } from 'antd'
+
+const Header = () => {
+    return (
+        <Layout.Header className="header">
+            <div className="header-logo">
+                <Link to="/">Getflix</Link>
+            </div>
+            <Menu className="header-menu" mode="horizontal">
+                <Menu.Item key="login">
+                    <Link to="/login">Login</Link>
+                </Menu.Item>
+                <Menu.Item key="signup">
+                    <Link to="/signup">Sign up</Link>
+                </Menu.Item>
+            </Menu>
+        </Layout.Header>
+    )
+}
+
+const HomePage = () => {
+    return 'home'
+}
+
+const LoginPage = () => {
+    return 'login'
+}
+
+const SignupPage = () => {
+    return 'signup'
+}
+
+const Content = () => {
+    return (
+        <Layout.Content>
+            <Switch>
+                <Route path="/login">
+                    <LoginPage />
+                </Route>
+                <Route path="/signup">
+                    <SignupPage />
+                </Route>
+                <Route path="/">
+                    <HomePage />
+                </Route>
+            </Switch>
+        </Layout.Content>
+    )
+}
+
+const MainLayout = () => {
+    return (
+        <Layout>
+            <Header />
+            <Content />
+        </Layout>
+    )
+}
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                Backend API URL: {config.apiUrl ?? 'null'} <br />
-                Environment: {process.env.NODE_ENV ?? 'null'} <br />
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"></a>
-            </header>
-        </div>
+        <Router>
+            <MainLayout />
+        </Router>
     )
 }
 
