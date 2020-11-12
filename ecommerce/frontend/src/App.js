9e1fa9af4f4b804166fdda4b0173ecffb1d68bc1
Mehdi Saffar <saffar.mehdi3@gmail.com>
2020-11-12 22:29:32 +0300

    Add category bar, footer

--- ecommerce/frontend/src/App.js
+++ ecommerce/frontend/src/App.js
@@ -2,7 +2,24 @@ import logo from './logo.svg'
 import './App.less'
 import config from './config.js'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
-import { Layout, Menu } from 'antd'
+import { Layout, Menu, Row, Col } from 'antd'
+
+const CategoryBar = ({ categories = [] }) => {
+    return (
+        <div>
+            <Menu mode="horizontal" className="category-menu">
+                {categories.map(({ key, title }) => {
+                    const url = `/categories/${key}`
+                    return (
+                        <Menu.Item className="category-menu__menu-item" key={key}>
+                            <Link to={url}>{title}</Link>
+                        </Menu.Item>
+                    )
+                })}
+            </Menu>
+        </div>
+    )
+}
 
 const Header = () => {
     return (
@@ -22,8 +39,52 @@ const Header = () => {
     )
 }
 
+const HomePage_Splash = () => {
+    return <div style={{ height: 400 }}></div>
+}
+
+const HomePage_MainContent = () => {
+    return <div style={{ height: 100 }}></div>
+}
+
+const Footer = () => {
+    const cols = [
+        [['About us'], ['My Account'], ['Orders History']],
+        [['Contact us'], ['Address'], ['Email'], ['Working Days']],
+        [['Follow Us'], ['Facebook'], ['Twitter'], ['Instagram']],
+    ]
+    return (
+        <Layout.Footer className="footer">
+            {cols.map((col, ix1) => {
+                return (
+                    <div key={ix1} className="footer-col">
+                        {col.map((entry, ix2) => {
+                            return <p key={ix2}>{entry}</p>
+                        })}
+                    </div>
+                )
+            })}
+        </Layout.Footer>
+    )
+}
+
 const HomePage = () => {
-    return 'home'
+    const categories = [
+        { key: 'electronics', title: 'Electronics' },
+        { key: 'home_and_garden', title: 'Home & Garden' },
+        { key: 'personal_care', title: 'Personal Care' },
+        { key: 'furniture', title: 'Furniture' },
+        { key: 'pet_supplies', title: 'Pet Supplies' },
+        { key: 'mens_fashion', title: "Men's Fashion" },
+        { key: 'womens_fashion', title: "Women's Fashion" },
+    ]
+    return (
+        <div>
+            <HomePage_Splash />
+            <CategoryBar categories={categories} />
+            <HomePage_MainContent />
+        </div>
+    )
 }
 
 const LoginPage = () => {
@@ -36,7 +97,7 @@ const SignupPage = () => {
 
 const Content = () => {
     return (
-        <Layout.Content>
+        <Layout.Content className="content">
             <Switch>
                 <Route path="/login">
                     <LoginPage />
@@ -57,6 +118,7 @@ const MainLayout = () => {
         <Layout>
             <Header />
             <Content />
+            <Footer />
         </Layout>
     )
 }
