8e73975964e3a69d28d8c448dfdf775383ed597c
Mehdi Saffar <saffar.mehdi3@gmail.com>
2020-11-13 13:52:59 +0300

    edit page title and favicon

--- ecommerce/frontend/src/App.js
+++ ecommerce/frontend/src/App.js
@@ -4,10 +4,14 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 import { AppContextProvider } from './context/AppContext'
 import { MainLayout } from './components/layout/MainLayout'
+import { Helmet } from 'react-helmet'
 
 function App() {
     return (
         <AppContextProvider>
+            <Helmet>
+                <title>Getflix - Get all you want</title>
+            </Helmet>
             <Router>
                 <MainLayout />
             </Router>
