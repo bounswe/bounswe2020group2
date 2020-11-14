45e9d99826b9a5b009adfb5fe46b0611c81bfdcc
karalleyna <36455180+karalleyna@users.noreply.github.com>
2020-11-14 03:18:39 +0300

    Recreate CategoryViewModel and rename it

---
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/viewmodels/CategoryViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.getflix.ui.fragment
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.example.getflix.models.CategoryModel
+import com.example.getflix.models.ProductModel
+import com.example.getflix.models.SubcategoryModel
+
+class CategoryViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _categories = MutableLiveData<HashMap<Int, CategoryModel>>()
+    val categories: LiveData<HashMap<Int, CategoryModel>>
+        get() = _categories
+
+    private val _displayedCategory = MutableLiveData<CategoryModel>()
+    val displayedCategory: LiveData<CategoryModel>
+        get() = _displayedCategory
+
+    fun setCategory(categoryId: Int) {
+        var b = ProductModel(1.toDouble(), "jn", "km", 22.toFloat(), "skds", "lkslkd")
+        var a = listOf<ProductModel>(b)
+        var subcategoryModel1 = SubcategoryModel(1, 2, "Computers", a)
+        var subcategoryModel2 = SubcategoryModel(2, 2, "Mobile Phones", a)
+        var subcategories = listOf<SubcategoryModel>(subcategoryModel1, subcategoryModel2)
+        var categoryModel = CategoryModel(1, "Electronics", subcategories)
+        _displayedCategory.value = categoryModel
+    }
+
+
+}
+
