89022cc7feead3ac285da5140b459471d7b6bf65
hande <hansirikci@gmail.com>
2020-11-14 22:38:06 +0300

    cart fragment connected to adapter

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragment/CartFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragment/CartFragment.kt
@@ -6,9 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.getflix.R
 import com.example.getflix.databinding.FragmentCartBinding
 import com.example.getflix.databinding.FragmentCategoriesBinding
+import com.example.getflix.ui.adapters.cartFragmentAdapter
+import kotlinx.android.synthetic.main.fragment_cart.*
 
 
 class CartFragment : Fragment() {
@@ -22,6 +25,17 @@ class CartFragment : Fragment() {
             container,false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+        recycler_view.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = LinearLayoutManager(activity)
+            // set the custom adapter to the RecyclerView
+            adapter = cartFragmentAdapter()
+        }
     }
 
 
