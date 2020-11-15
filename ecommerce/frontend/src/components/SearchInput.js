669b5191d1ec670fe362bbe6db703668c7fc4ccd
Abdullah Yildiz <abdullahyildiz321@gmail.com>
2020-11-15 15:52:53 +0300

    [FRONTEND] Add Search Input component (#143)
    * add Search Input component
    
    * refactoring, const-methods moved inside component
    
    * refactoring, delete unnecessary onSearchInput, onSearch prop default arrow func
    
    Co-authored-by: yldz7887 <yldz-7887@windowslive.com>

---
+++ ecommerce/frontend/src/components/SearchInput.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import './SearchInput.less'
+import { Select, Input } from 'antd'
+
+const { Option } = Select
+const { Search } = Input
+
+// TODO - Fetch from a common file or backend
+const categories = [
+    { key: 'electronics', title: 'Electronics' },
+    { key: 'home_and_garden', title: 'Home & Garden' },
+    { key: 'personal_care', title: 'Personal Care' },
+    { key: 'furniture', title: 'Furniture' },
+    { key: 'pet_supplies', title: 'Pet Supplies' },
+    { key: 'mens_fashion', title: "Men's Fashion" },
+    { key: 'womens_fashion', title: "Women's Fashion" },
+]
+
+export const SearchInput = ({ width = 120, onSearch = () => {} }) => {
+    const [category, setCategory] = useState('Electronics')
+
+    function handleCategoryChange(value) {
+        setCategory(value)
+    }
+
+    const categoryDropdown = (
+        <Select defaultValue={category} style={{ width }} onChange={handleCategoryChange}>
+            {categories.map(category => (
+                <Option value={category.title} key={category.key}>
+                    {category.title}
+                </Option>
+            ))}
+        </Select>
+    )
+
+    return (
+        <div className="search-input">
+            {categoryDropdown}
+            <Search
+                placeholder="Search for products, categories and brands"
+                onSearch={value => {
+                    if (value.trim() === '') {
+                        return
+                    }
+                    onSearch(value.trim())
+                }}
+                enterButton
+            />
+        </div>
+    )
+}
