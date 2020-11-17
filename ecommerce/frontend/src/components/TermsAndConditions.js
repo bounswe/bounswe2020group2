20f7e8e262f10e6ac01abf91a230333d721aa504
ozdenizdolu <32651175+ozdenizdolu@users.noreply.github.com>
2020-11-17 16:48:29 +0300

    [F] Add the signup form component (#137)
    * Create a branch for making signup form
    * implemented the SignUpForm.js
    * correction on some names (made them camelCase)
    * debugging purposes
    * Signup form enhancements and bug fixes
    - Give initial value to register as customer radio button
    - Fix error when switching from customer to vendor
    - Add simple recaptcha
    - Add validation to phone country code and number
    - Make phone number an InputNumber
    - Correctly hide (but preserve) input fields of Vendor only
    * Changes that have been made:
    - Zip code can only be a number
    - User agreement Modal pops up when clicked. 
    - Minor corrections on typos.
    * Finalizing the pull request reviews:
    - Added name & surname fields
    - Doesn't show double error prompt when a field is left blank.
    - <App /> added back to the index.js
    
    Co-authored-by: Özdeniz Dolu <ozdendolu@windowslive.com>
    Co-authored-by: Mehdi Saffar <saffar.mehdi3@gmail.com>

---
+++ ecommerce/frontend/src/components/TermsAndConditions.js
@@ -0,0 +1,16 @@
+export const TermsAndConditions = () => {
+    return (
+        <>
+            <h1>Terms and Conditions for Getflix</h1>
+
+            <h2>Introduction</h2>
+
+            <p>These Website Standard Terms and Conditions written on this webpage shall manage your use of our website, getflix accessible at getflix.com.</p>
+
+            <p>These Terms will be applied fully and affect to your use of this Website. By using this Website, you agreed to accept all terms and conditions written in here. You must not use this Website if you disagree with any of these Website Standard Terms and Conditions. These Terms and Conditions have been generated with the help of the <a href="https://www.termsandcondiitionssample.com">Terms And Conditiions Sample</a> and the <a href="https://www.generateprivacypolicy.com">Privacy Policy Generator</a>.</p>
+
+            <p>Minors or people below 18 years old are not allowed to use this Website.</p>
+           
+        </>
+    )
+}
