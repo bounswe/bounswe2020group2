e499fdefb1b230497290a74575952ae6b02108a5
Asli Aykan <asliayk@hotmail.com>
2020-11-17 18:06:28 +0300

    Revert "Delete CategoriesFragment.kt"
    This reverts commit 4c6a7b38f1842201beed58ce5774f5a52d0731ad.

---
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragment/CategoriesFragment.kt
@@ -0,0 +1,51 @@
+package com.example.getflix.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ExpandableListView
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.getflix.R
+import com.example.getflix.databinding.FragmentCategoriesBinding
+import com.example.getflix.model.CategoryModel
+import com.example.getflix.model.SubcategoryModel
+import com.example.getflix.ui.adapter.CategoriesAdapter
+import com.thoughtbot.expandablerecyclerview.listeners.OnGroupClickListener
+
+
+class CategoriesFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentCategoriesBinding>(
+            inflater, R.layout.fragment_categories,
+            container, false
+        )
+
+        var cats = listOf<CategoryModel>(
+            CategoryModel(
+                1, "Home",
+                listOf(SubcategoryModel(2, 1, "Furniture"))
+            ), CategoryModel(
+                2, "Electronics",
+                listOf(SubcategoryModel(2, 2, "Computers"))
+            )
+        )
+        //var catss = listOf(CategoryModel(1,cats[0].name, cats[0].subCats))
+        val adapter = CategoriesAdapter(cats)
+        binding.catRec.adapter = adapter
+
+
+
+        return binding.root
+    }
+
+
+
+
+}
