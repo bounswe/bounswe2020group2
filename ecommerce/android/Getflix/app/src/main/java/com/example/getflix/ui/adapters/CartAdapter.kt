376112da7a5449ae182dda2bb32c21ccad4f7512
Asli Aykan <asliayk@hotmail.com>
2020-11-18 12:41:39 +0300

    data binding
    data binding added.

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/adapters/CartAdapter.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/adapters/CartAdapter.kt
@@ -1,78 +1,46 @@
 package com.example.getflix.ui.adapters
 
-import android.content.Intent
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.getflix.R
-import com.example.getflix.activities.productDetailActivity
+import com.example.getflix.databinding.ProductCardBinding
+import com.example.getflix.models.ProductModel
 
-class CartAdapter : RecyclerView.Adapter<CartAdapter.ViewHolder>() {
+class CartAdapter(
+    private val productList: ArrayList<ProductModel>?,
+)  : RecyclerView.Adapter<CartAdapter.RowHolder>() {
 
-    private val name = arrayOf("T-Shirt",
-        "Skirt", "Sweater", "Jean",
-        "Chair", "Table", "Desk",
-        "Notebook")
 
-    private val price = arrayOf("30$", "40$",
-        "30$", "50$",
-        "60$", "70$",
-        "60$", "10$")
+    class RowHolder(val binding: ProductCardBinding) : RecyclerView.ViewHolder(binding.root) {
 
-    private val brandId = arrayOf("GAP",
-        "GAP", "MAVI", "MAVI",
-        "IKEA", "IKEA", "IKEA",
-        "NEZIH")
+        fun bind(product: ProductModel, position: Int) {
+            binding.product = product
+        }
 
-    private val description = arrayOf("Blue T-Shirt",
-        "Blue Skirt", "Blue Sweater", "Blue Jean",
-        "Blue Chair", "Blue Table", "Blue Desk",
-        "Blue Notebook")
-
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        var name: TextView
-        var price: TextView
-        var brandId: TextView
-        var description: TextView
-
-        init {
-            name = itemView.findViewById(R.id.name)
-            price = itemView.findViewById(R.id.price)
-            brandId = itemView.findViewById(R.id.brand)
-            description = itemView.findViewById(R.id.description)
-
-            itemView.setOnClickListener {
-                var position: Int = getAdapterPosition()
-                val context = itemView.context
-                val intent = Intent(context, productDetailActivity::class.java).apply {
-                    putExtra("name", name.text)
-                    putExtra("price", price.text)
-                    putExtra("brandId", brandId.text)
-                    putExtra("description", description.text)
-                }
-                context.startActivity(intent)
+        companion object {
+            fun from(parent: ViewGroup): RowHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ProductCardBinding.inflate(layoutInflater,parent,false)
+                return RowHolder(binding)
             }
         }
+
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
-        val v = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.product_card, viewGroup, false)
-        return ViewHolder(v)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartAdapter.RowHolder {
+        return RowHolder.from(parent)
     }
 
-    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
-        viewHolder.name.text = name[i]
-        viewHolder.price.text = price[i]
-        viewHolder.brandId.text = brandId[i]
-        viewHolder.description.text = description[i]
-
+    override fun onBindViewHolder(holder: RowHolder, position: Int) {
+        productList?.get(position)?.let { holder.bind(it, position) }
     }
 
     override fun getItemCount(): Int {
-        return name.size
+        if (productList != null) {
+            return productList.count()
+        }
+        return 0
     }
+
+
 }
--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragment/CartFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragment/CartFragment.kt
@@ -7,9 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.getflix.R
 import com.example.getflix.databinding.FragmentCartBinding
+import com.example.getflix.models.ProductModel
 import com.example.getflix.ui.adapters.CartAdapter
+import com.example.getflix.ui.adapters.FavoritesAdapter
 import kotlinx.android.synthetic.main.fragment_cart.*
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -28,19 +31,19 @@ class CartFragment : Fragment() {
 
         activity?.toolbar!!.toolbar_title.text = getString(R.string.cart)
 
+        val recView = binding?.cartList as RecyclerView
+        val products = arrayListOf(
+            ProductModel(1,"iPhone","Electronics",null,null,null),
+            ProductModel(1,"Bag","Electronics",null,null,null)
+        )
+        val productListAdapter = CartAdapter(products)
+        recView.adapter = productListAdapter
+        recView.setHasFixedSize(true)
+
         return binding.root
     }
 
-    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(itemView, savedInstanceState)
-        recycler_view.apply {
-            // set a LinearLayoutManager to handle Android
-            // RecyclerView behavior
-            layoutManager = LinearLayoutManager(activity)
-            // set the custom adapter to the RecyclerView
-            adapter = CartAdapter()
-        }
-    }
+
 
 
 }
