a1607751195ba18045be24b9cfabe378551ef367
Asli Aykan <asliayk@hotmail.com>
2020-11-18 12:20:54 +0300

    resolving conflicts

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/models/ProductModel.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/models/ProductModel.kt
@@ -1,10 +1,10 @@
 package com.example.getflix.models
 
 data class ProductModel(
-    var id: Double,
+    var id: Int,
     var name: String,
     var category: String,
-    var price: Float,
-    var brandId: String,
-    var description: String
+    var price: Float?,
+    var brandId: String?,
+    var description: String?
 )
