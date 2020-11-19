e968d3d6c89f73d24af4aab690e36ba7ad08b3a0
Mehdi Saffar <saffar.mehdi3@gmail.com>
2020-11-19 13:22:49 +0300

    Properly doing query params
    - Refactor query params handling
    - Spacing pagination from product grid
    - Add all pagination props

--- ecommerce/frontend/src/components/pages/SearchPage.js
+++ ecommerce/frontend/src/components/pages/SearchPage.js
@@ -31,21 +31,32 @@ export const SearchPage = () => {
 }
 
 export const _SearchPage = ({ initialValues = {} }) => {
-    const initialQuery = { query: initialValues.query }
-    const initialFilters = R.omit('query', initialValues)
+    const defaultPageSize = 10
+    const values = {
+        query: initialValues.query,
+        filters: R.omit('query', initialValues),
+        pagination: {
+            pageSize: defaultPageSize,
+            ...R.pick(['current', 'pageSize'], initialValues),
+        },
+    }
 
     const history = useHistory()
 
     const [isLoading, setIsLoading] = useState(true)
+
     const [products, setProducts] = useState([])
+    // total number of products matching the query in the database, NOT returned by backende([])
+    const [total, setTotal] = useState(0)
 
     useEffect(() => {
         async function fetch() {
             try {
                 setIsLoading(true)
                 await sleep(1000)
+                const total = 50
                 setProducts(
-                    [...Array(10)].map(x => {
+                    [...Array(total)].map(x => {
                         return {
                             title: 'Title',
                             rating: '5',
@@ -57,6 +68,7 @@ export const _SearchPage = ({ initialValues = {} }) => {
                         }
                     }),
                 )
+                setTotal(total)
 
                 notification.success({ description: 'Search results ready!' })
             } catch {
@@ -69,35 +81,43 @@ export const _SearchPage = ({ initialValues = {} }) => {
         fetch()
     }, [])
 
-    const onSubmitFilters = newFilters => {
-        const queryParams = { ...newFilters, ...initialQuery }
+    const refreshSearchWith = queryParams => {
         history.push({
             pathname: '/search',
-            search: '?' + qs.stringify(queryParams, { arrayFormat: 'comma' }),
+            search:
+                '?' +
+                qs.stringify(
+                    { ...queryParams.filters, query: queryParams.query, ...queryParams.pagination },
+                    { arrayFormat: 'comma' },
+                ),
         })
     }
 
-    const onSearch = newQuery => {
-        const queryParams = { ...initialFilters, ...{ query: newQuery } }
-        history.push({
-            pathname: '/search',
-            search: '?' + qs.stringify(queryParams, { arrayFormat: 'comma' }),
-        })
-    }
+    const onSubmitFilters = filters => refreshSearchWith({ ...values, filters })
+    const onSearch = query => refreshSearchWith({ ...values, query })
+    const onPaginationChanged = (current, pageSize) =>
+        refreshSearchWith({ ...values, pagination: { ...values.pagination, current, pageSize } })
 
     return (
         <Form.Provider onFormFinish={console.log}>
             <div className={'search-page'}>
                 <div className="search-page-bar">
-                    <SearchInput initialValue={initialValues.query} onSearch={onSearch} />
+                    <SearchInput initialValue={values.query} onSearch={onSearch} />
                 </div>
                 <div className="search-page-main">
                     <div className="search-page-side-panel">
-                        <SearchSidePanel initialValues={initialValues} onSubmit={onSubmitFilters} />
+                        <SearchSidePanel initialValues={values.filters} onSubmit={onSubmitFilters} />
                     </div>
                     <div className="search-page-results">
                         <Spin spinning={isLoading}>
-                            <SearchResults products={products} />
+                            <SearchResults
+                                products={products}
+                                pagination={{
+                                    ...values.pagination,
+                                    total,
+                                }}
+                                onPaginationChanged={onPaginationChanged}
+                            />
                         </Spin>
                     </div>
                 </div>
--- ecommerce/frontend/src/components/search/SearchResults.js
+++ ecommerce/frontend/src/components/search/SearchResults.js
@@ -7,13 +7,20 @@ import { ProductCard } from '../product_card/ProductCard'
 
 export const SearchResults = ({ products, pagination, onPaginationChanged }) => {
     return (
-        <div>
-            <div className="product-grid">
+        <div className="search-results">
+            <div className="search-results-product-grid">
                 {products.map(product => (
                     <ProductCard key={product.productId} product={product} />
                 ))}
             </div>
-            <Pagination pagination={pagination} onChange={onPaginationChanged} hideOnSinglePage />
+            <Pagination
+                className="search-results-pagination"
+                {...pagination}
+                onChange={onPaginationChanged}
+                showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
+                showSizeChanger
+                showQuickJumper
+            />
         </div>
     )
 }
