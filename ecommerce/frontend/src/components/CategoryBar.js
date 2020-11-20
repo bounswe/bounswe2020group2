0fd38fa5ab49bfb1f10a688c4e1b9989b03d34e6
Özdeniz Dolu <ozdendolu@windowslive.com>
2020-11-20 18:02:02 +0300

    Make the categoryBar consist of many dropdown menus with subcategories
    *categoryBar object no longer takes props, it directly pulls the categories from utils.js
    
    *user can both click on the category names and subcategories

--- ecommerce/frontend/src/components/CategoryBar.js
+++ ecommerce/frontend/src/components/CategoryBar.js
@@ -1,16 +1,31 @@
 import { Menu } from 'antd'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import './CategoryBar.less'
+import { categories, subcategories } from '../utils.js'
+import SubMenu from 'antd/lib/menu/SubMenu'
 
-export const CategoryBar = ({ categories = [] }) => {
+export const CategoryBar = () => {
+    const history = useHistory()
+
+    const onCategoryClick = url => () => {
+        history.push({pathname: url})
+    }
+
     return (
         <Menu mode="horizontal" className="category-menu">
-            {categories.map(({ key, title }) => {
-                const url = `/categories/${key}`
+            {Object.entries(categories).map(([categoryKey, categoryTitle]) => {
+                const categoryURL = `/categories/${categoryKey}`
                 return (
-                    <Menu.Item className="category-menu__menu-item" key={key}>
-                        <Link to={url}>{title}</Link>
-                    </Menu.Item>
+                    <SubMenu onTitleClick={onCategoryClick(categoryURL)} title={categoryTitle} className="category-menu__menu-item" key={categoryKey}>
+                        {Object.entries(subcategories[categoryKey]).map(([subCategoryKey, subCategoryTitle]) => {
+                            const subCategoryURL = categoryURL + `/${subCategoryKey}`
+                            return (
+                                <Menu.Item key={subCategoryKey} >
+                                    <Link to={subCategoryURL}>{subCategoryTitle}</Link>  
+                                </Menu.Item>
+                            )
+                        })}
+                    </SubMenu>
                 )
             })}
         </Menu>
