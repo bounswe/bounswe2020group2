0a41fe1056220ca8c891322f49c5d8f164021a8f
vaveylafv <44326869+vaveylafv@users.noreply.github.com>
2020-11-20 19:55:07 +0300

    formatting

--- ecommerce/frontend/src/components/pages/HomePage.js
+++ ecommerce/frontend/src/components/pages/HomePage.js
@@ -40,13 +40,13 @@ const HomePage_Splash = () => {
 
 const HomePage_MainContent = () => {
     const images = [
-        "https://stylecaster.com/wp-content/uploads/2018/10/bag-storage.jpg",
-        "https://cdn.pixabay.com/photo/2019/07/13/13/42/watch-4334815_960_720.jpg",
-        "https://upload.wikimedia.org/wikipedia/commons/6/65/Product_Photography.jpg",
-        "https://images.unsplash.com/photo-1526947425960-945c6e72858f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwCP24CO6WEGAUhkfVg4ozqfeve3pvkhEEZg&usqp=CAU"
+        'https://stylecaster.com/wp-content/uploads/2018/10/bag-storage.jpg',
+        'https://cdn.pixabay.com/photo/2019/07/13/13/42/watch-4334815_960_720.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/6/65/Product_Photography.jpg',
+        'https://images.unsplash.com/photo-1526947425960-945c6e72858f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwCP24CO6WEGAUhkfVg4ozqfeve3pvkhEEZg&usqp=CAU',
     ]
-    return ( 
+    return (
         <div className="trending-grid-wrapper">
             <TrendingGrid imageUrls={images} />
         </div>
