ba8301a693311eebfbbf4ef93593710e1ec47e9e
Furkan Nane <furkannane6@gmail.com>
2020-11-21 18:18:48 +0300

    Cleaning a bit
    Cleaning a bit

--- ecommerce/backend/API/views/products.py
+++ ecommerce/backend/API/views/products.py
@@ -1,11 +1,7 @@
-from rest_framework import exceptions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.parsers import JSONParser
 
 from API.utils import permissions, Role
-from API.utils.jwttoken import generate_access_token
-from API.utils.crypto import Crypto
 from API.models.product import Product
 from API.model_serializers import ProductSerializer
 
