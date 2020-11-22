d6aa41946f15394923b11b5d0b4b56f704680758
Mehdi Saffar <saffar.mehdi3@gmail.com>
2020-11-22 16:23:33 +0300

    [FRONTEND] Axios instance, token management, login logout templates (#185)
    * add init login logout interceptors
    * add bearer prefix in auth header
    * no init if no token

--- ecommerce/frontend/src/config.js
+++ ecommerce/frontend/src/config.js
@@ -1,3 +1,3 @@
-export default {
-    apiUrl: process.env.REACT_APP_API_URL ?? 'http://localhost:5000',
+export const config = {
+    apiUrl: process.env.REACT_APP_API_URL ?? 'http://localhost:8000',
 }
