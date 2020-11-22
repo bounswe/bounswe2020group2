4549ab2ed8b7ce26380a95a26320880dd77451dd
Mehdi Saffar <saffar.mehdi3@gmail.com>
2020-11-22 13:50:43 +0300

    change to search products

--- ecommerce/frontend/src/components/CategoryBar.js
+++ ecommerce/frontend/src/components/CategoryBar.js
@@ -3,25 +3,45 @@ import { Link, useHistory } from 'react-router-dom'
 import './CategoryBar.less'
 import { categories, subcategories } from '../utils.js'
 import SubMenu from 'antd/lib/menu/SubMenu'
+import qs from 'querystring'
 
 export const CategoryBar = () => {
     const history = useHistory()
 
-    const onCategoryClick = url => () => {
-        history.push({pathname: url})
+    const onCategoryClick = category => () => {
+        history.push({
+            pathname: `/search/products`,
+            search: qs.stringify({
+                category,
+            }),
+        })
+    }
+
+    const onSubcategoryClick = (category, subcategory) => () => {
+        history.push({
+            pathname: `/search/products`,
+            search: qs.stringify({
+                category,
+                subcategory,
+            }),
+        })
     }
 
     return (
         <Menu mode="horizontal" className="category-menu">
             {Object.entries(categories).map(([categoryKey, categoryTitle]) => {
-                const categoryURL = `/categories/${categoryKey}`
                 return (
-                    <SubMenu onTitleClick={onCategoryClick(categoryURL)} title={categoryTitle} className="category-menu__menu-item" key={categoryKey}>
+                    <SubMenu
+                        onTitleClick={onCategoryClick(categoryKey)}
+                        title={categoryTitle}
+                        className="category-menu__menu-item"
+                        key={categoryKey}>
                         {Object.entries(subcategories[categoryKey]).map(([subCategoryKey, subCategoryTitle]) => {
-                            const subCategoryURL = categoryURL + `/${subCategoryKey}`
                             return (
-                                <Menu.Item key={subCategoryKey} >
-                                    <Link to={subCategoryURL}>{subCategoryTitle}</Link>  
+                                <Menu.Item
+                                    key={subCategoryKey}
+                                    onClick={onSubcategoryClick(categoryKey, subCategoryKey)}>
+                                    {subCategoryTitle}
                                 </Menu.Item>
                             )
                         })}
