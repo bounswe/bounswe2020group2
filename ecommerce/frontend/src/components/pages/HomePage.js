dcc6421ee918392949b989aed68c93beb55cfb08
Mehdi Saffar <saffar.mehdi3@gmail.com>
2020-11-22 20:31:06 +0300

    change productId to id, add formatProduct in utils, api for trending products

--- ecommerce/frontend/src/components/pages/HomePage.js
+++ ecommerce/frontend/src/components/pages/HomePage.js
@@ -3,11 +3,13 @@ import { CategoryBar } from '../CategoryBar'
 import { TrendingGrid } from '../TrendingGrid'
 import getflixLogo from '../../assets/logo.png'
 import { useAppContext } from '../../context/AppContext'
+import { useEffect, useState } from 'react'
+import { Spin } from 'antd'
+import { formatProduct, productSortBy, sleep } from '../../utils'
+import { api } from '../../api'
 
 export const HomePage = () => {
     // example usage
-    const { user, setUser } = useAppContext()
-
     return (
         <div>
             <HomePage_Splash />
@@ -30,59 +32,80 @@ const HomePage_Splash = () => {
 }
 
 const HomePage_MainContent = () => {
-    const products = [
-        {
-            title: 'Purse',
-            rating: '5',
-            price: '30.00',
-            currency: 'TL',
-            imageUrl: 'https://stylecaster.com/wp-content/uploads/2018/10/bag-storage.jpg',
-            width: 300,
-            productId: 1324,
-        },
-        {
-            title: 'Watch',
-            rating: '5',
-            price: '45.00',
-            currency: 'TL',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/07/13/13/42/watch-4334815_960_720.jpg',
-            width: 300,
-            productId: 2123,
-        },
-        {
-            title: 'onlar iyi',
-            rating: '5',
-            price: '30.00',
-            currency: 'TL',
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/65/Product_Photography.jpg',
-            width: 300,
-            productId: 32345,
-        },
-        {
-            title: 'fena biji',
-            rating: '5',
-            price: '45.00',
-            currency: 'TL',
-            imageUrl:
-                'https://images.unsplash.com/photo-1526947425960-945c6e72858f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
-            width: 300,
-            productId: 4435,
-        },
-        {
-            title: 'onlar iyi',
-            rating: '5',
-            price: '96.00',
-            currency: 'TL',
-            imageUrl:
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwCP24CO6WEGAUhkfVg4ozqfeve3pvkhEEZg&usqp=CAU',
-            width: 300,
-            productId: 5678,
-        },
-    ]
+    const [trendingProducts, setTrendingProducts] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        async function fetch() {
+            try {
+                setIsLoading(true)
+
+                // once backend works we will uncomment the following
+                // const { data } = await api.get(`/products/homepage/${5}`)
+                // let { products } = data
+                // products = products.map(formatProduct)
+                // setTrendingProducts(products)
+
+                await sleep(1000)
+                setTrendingProducts([
+                    {
+                        title: 'Purse',
+                        rating: '5',
+                        price: '30.00',
+                        currency: 'TL',
+                        imageUrl: 'https://stylecaster.com/wp-content/uploads/2018/10/bag-storage.jpg',
+                        id: 1324,
+                    },
+                    {
+                        title: 'Watch',
+                        rating: '5',
+                        price: '45.00',
+                        currency: 'TL',
+                        imageUrl: 'https://cdn.pixabay.com/photo/2019/07/13/13/42/watch-4334815_960_720.jpg',
+                        id: 2123,
+                    },
+                    {
+                        title: 'onlar iyi',
+                        rating: '5',
+                        price: '30.00',
+                        currency: 'TL',
+                        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/65/Product_Photography.jpg',
+                        id: 32345,
+                    },
+                    {
+                        title: 'fena biji',
+                        rating: '5',
+                        price: '45.00',
+                        currency: 'TL',
+                        imageUrl:
+                            'https://images.unsplash.com/photo-1526947425960-945c6e72858f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+                        id: 4435,
+                    },
+                    {
+                        title: 'onlar iyi',
+                        rating: '5',
+                        price: '96.00',
+                        currency: 'TL',
+                        imageUrl:
+                            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwCP24CO6WEGAUhkfVg4ozqfeve3pvkhEEZg&usqp=CAU',
+                        id: 5678,
+                    },
+                ])
+            } catch (error) {
+                console.error('failed to load trending products', error)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+        fetch()
+    }, [])
 
     return (
         <div className="trending-grid-wrapper">
-            <TrendingGrid trendingProducts={products} />
+            <Spin spinning={isLoading}>
+                <TrendingGrid trendingProducts={trendingProducts} />
+            </Spin>
         </div>
     )
 }
--- ecommerce/frontend/src/context/AppContext.js
+++ ecommerce/frontend/src/context/AppContext.js
@@ -9,7 +9,7 @@ const guestUser = { type: 'guest' }
 const customerUser = { type: 'customer', name: 'Mehdi', surname: 'Saffar', email: 'example@gmail.com' }
 
 function useApp() {
-    const [user, setUser] = useState(customerUser)
+    const [user, setUser] = useState(guestUser)
     const [requestInterceptorId, setRequestInterceptorId] = useState(null)
 
     const requestInterceptor = request => {
