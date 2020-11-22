c380918740bcb995a0a3d4f5ca5f1d613e64dedd
Özdeniz Dolu <ozdendolu@windowslive.com>
2020-11-22 20:17:25 +0300

    correction for Mehdi's review comment

--- ecommerce/frontend/src/components/pages/SignupPage.js
+++ ecommerce/frontend/src/components/pages/SignupPage.js
@@ -1,12 +1,10 @@
 import { SignUpForm } from '../SignUpForm'
 import './SignupPage.less'
 export const SignupPage = () => {
-    return <>
-        <div className="signup-container">  
+    return <div className="signup-container">  
             <div className="signup-header">
                 <h1>Signup to Getflix!</h1>
             </div>  
             <SignUpForm />
         </div>
-    </>
 }
