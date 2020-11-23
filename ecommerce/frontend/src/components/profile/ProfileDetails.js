e6a74c9cdbe66610aafa67259955734a51179f56
vaveylafv <44326869+vaveylafv@users.noreply.github.com>
2020-11-23 17:22:33 +0300

    profile details converted to tabs

--- ecommerce/frontend/src/components/profile/ProfileDetails.js
+++ ecommerce/frontend/src/components/profile/ProfileDetails.js
@@ -1,23 +1,20 @@
 import './ProfileDetails.less'
 import { Link } from 'react-router-dom'
+import { Tabs } from 'antd'
+
+const { TabPane } = Tabs;
 
 export const ProfileDetails = () => {
     return (
         <div className="profile-details">
-            <ul className="profile-details-action-list">
-                <li>
-                    {' '}
-                    <Link to="/profile/change-password">Change Password</Link>{' '}
-                </li>
-                <li>
-                    {' '}
-                    <Link to="/profile/purchases">Purchase History</Link>{' '}
-                </li>
-                <li>
-                    {' '}
-                    <Link to="/profile/messages">Messages</Link>{' '}
-                </li>
-            </ul>
+            
+            <Tabs className="profile-tabpane" tabPosition="left" type="card" defaultActiveKey="4" centered>
+                <TabPane tab={<Link to="/profile/change-password">Change Password</Link>} key="1" />
+                <TabPane tab={<Link to="/profile/purchases">Purchase History</Link>} key="2" />
+                <TabPane tab={<Link to="/profile/messages">Messages</Link>} key="3" />
+                <TabPane tab="" key="4" disabled />
+            </Tabs>
+            
         </div>
     )
 }
