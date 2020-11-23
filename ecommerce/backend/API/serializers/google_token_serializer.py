ec7792d3b33be7e6906bb5c8954aed80f0474bf6
Burak Çetin <burak.cetin@boun.edu.tr>
2020-11-23 23:00:43 +0300

    to merge

--- ecommerce/backend/API/serializers/google_token_serializer.py
+++ ecommerce/backend/API/serializers/google_token_serializer.py
@@ -2,5 +2,3 @@ from rest_framework import serializers
 
 class GoogleTokenSerializer(serializers.Serializer):
     id_token = serializers.CharField(max_length=32)
-class GoogleResponseSerializer(serializers.Serializer):
-    id_token = serializers.CharField(max_length=32)
--- ecommerce/backend/API/views/google_login.py
+++ ecommerce/backend/API/views/google_login.py
@@ -22,14 +22,14 @@ def googlelogin(request):
     serializer = google_token_serializer.GoogleTokenSerializer(data=data)
     print(serializer)
     if not serializer.is_valid():
-        return Response("Invalid input")
-    crypto = Crypto()
-    token = serializer.validated_data.get("id_token")
+        return Response("Invalid input from frontend.")
+    #token = serializer.validated_data.get("id_token")
+    token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImRlZGMwMTJkMDdmNTJhZWRmZDVmOTc3ODRlMWJjYmUyM2MxOTcyNGQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNzgwNjUwNjU1NjIwLThxaTVlcjYwOTRvdWlybGI2NmIyYzBobTZobGZvOXM4LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNzgwNjUwNjU1NjIwLThxaTVlcjYwOTRvdWlybGI2NmIyYzBobTZobGZvOXM4LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA5MjEwNTQ2NjY5NDExNDA2Njg0IiwiZW1haWwiOiJzYWZmYXIubWVoZGkzQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiNUlJcjJoclpva1piOFpxQ1lzUGFNZyIsIm5hbWUiOiJNZWhkaSBTYWZmYXIiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EtL0FPaDE0R2p4MjlKc21ZTUpfeV9CR0lWSEJJVGRvRVBjVHJHdmxRWnhQN3BfQnBzPXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6Ik1laGRpIiwiZmFtaWx5X25hbWUiOiJTYWZmYXIiLCJsb2NhbGUiOiJlbiIsImlhdCI6MTYwNjEzOTEzMSwiZXhwIjoxNjA2MTQyNzMxLCJqdGkiOiI3MDJmNjcxZTYzMjI0NWFiNTgzMGYyYmMyZDdhNGI2NzAxYjAzZjMxIn0.kmQPtIU6zHgZ_yQH38MPyvO0ZgYzbXmDrvmPSEpCpaZLEm6OEKWNM8IQUp3LjPdu3t-jP9ftgtvR6ijUBn2XWFYIEWqLNeZ37z2n3K2G0OAtRtu1hLBH5tQSs_kO3om6y2wQtZHpV81fMH1vpce6Lyh0Hqk_K56NtnfD8q5hzyjsI-rQGhNcPTOxTwRaqIEj79vDcgY75Zi37SwSlfWvYYk7YXqug4HPBaMPwb6IRny0o5frqZPE7neX3bv-yld_Yd6e_Furm9xB12enI93_ihpJUxX2tQtxelLFh5pHkEEy0EOBQMfo5Hq_ZBGfcXY3WTr8vjWxmE1Be-dPcOilIg"
     idinfo = id_token.verify_oauth2_token(token, requests.Request(), "780650655620-8qi5er6094ouirlb66b2c0hm6hlfo9s8.apps.googleusercontent.com")
     creds = google.oauth2.credentials.Credentials(token)
     authed_session = AuthorizedSession(creds)
     response = authed_session.get('https://openidconnect.googleapis.com/v1/userinfo')
-	user = User.objects.filter(email=response["email"]).first()
+    user = User.objects.filter(email=response["email"]).first()
     print(response);
     if computed_hash == user.password_hash:
         resp_serializer = google_token_serializer.LoginResponseSerializer(
@@ -38,6 +38,6 @@ def googlelogin(request):
                         'message': "Kullanıcı adı ya da şifre yanlış"
             }
         )
-		return Response(user_serializer.data)
+        return Response(user_serializer.data)
     else :
         return Response("Invalid Credentials")
