2e29d8e565e1028a649d01945fc7e6b32b7c6ebb
Burak Çetin <burak.cetin@boun.edu.tr>
2020-11-23 23:20:19 +0300

    Untested version

--- ecommerce/backend/API/serializers/google_token_serializer.py
+++ ecommerce/backend/API/serializers/google_token_serializer.py
@@ -1,4 +1,4 @@
 from rest_framework import serializers
 
 class GoogleTokenSerializer(serializers.Serializer):
-    id_token = serializers.CharField(max_length=32)
+    id_token = serializers.CharField(max_length=2000)
