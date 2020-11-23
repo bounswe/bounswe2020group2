6749233f4650121e454ed9518cf190f744cd9f6e
Mehdi Saffar <saffar.mehdi3@gmail.com>
2020-11-23 09:17:50 +0300

    db params from env, change requirements, update dockerfile

--- ecommerce/frontend/src/components/pages/SignupPage.js
+++ ecommerce/frontend/src/components/pages/SignupPage.js
@@ -1,10 +1,12 @@
 import { SignUpForm } from '../SignUpForm'
 import './SignupPage.less'
 export const SignupPage = () => {
-    return <div className="signup-container">  
+    return (
+        <div className="signup-container">
             <div className="signup-header">
                 <h1>Signup to Getflix!</h1>
-            </div>  
+            </div>
             <SignUpForm />
         </div>
+    )
 }
