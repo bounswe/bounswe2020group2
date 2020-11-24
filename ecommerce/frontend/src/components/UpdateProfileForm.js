04acc1aa44b3f760f7efaa85429c9889b5a2fd9d
vaveylafv <44326869+vaveylafv@users.noreply.github.com>
2020-11-24 11:40:37 +0300

    unknown information removed from update prof form

--- ecommerce/frontend/src/components/UpdateProfileForm.js
+++ ecommerce/frontend/src/components/UpdateProfileForm.js
@@ -79,9 +79,7 @@ export const UpdateProfileForm = ({ onSubmit = () => {}, user }) => {
             initialValues={{
                 name: user.name,
                 surname: user.lastname,
-                username: user.username,
                 email: user.email,
-                phone: user.phone,
             }}
             scrollToFirstError>
             {/* Name input */}
@@ -112,27 +110,6 @@ export const UpdateProfileForm = ({ onSubmit = () => {}, user }) => {
                 <Input />
             </Form.Item>
 
-            {/* Username input */}
-            <Form.Item
-                name="username"
-                label={
-                    <span>
-                        Username&nbsp;
-                        <Tooltip title="What do you want others to call you?">
-                            <QuestionCircleOutlined />
-                        </Tooltip>
-                    </span>
-                }
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your username!',
-                        whitespace: true,
-                    },
-                ]}>
-                <Input />
-            </Form.Item>
-
             {/* Email input */}
             <Form.Item
                 name="email"
@@ -148,109 +125,6 @@ export const UpdateProfileForm = ({ onSubmit = () => {}, user }) => {
                     },
                 ]}>
                 <Input />
-            </Form.Item>
-
-            {/* Zip code input */}
-            {user.userType === 'vendor' && (
-                <Form.Item
-                    name={['address', 'zipCode']}
-                    label="Zip Code"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please enter your zip code!',
-                        },
-                    ]}>
-                    <InputNumber style={{ width: '100%' }} />
-                </Form.Item>
-            )}
-
-            {/* City input */}
-            {user.userType === 'vendor' && (
-                <Form.Item
-                    name={['address', 'city']}
-                    label="City"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please select your city!',
-                        },
-                    ]}>
-                    <Input />
-                </Form.Item>
-            )}
-
-            {/* State input */}
-            {user.userType === 'vendor' && (
-                <Form.Item
-                    name={['address', 'state']}
-                    label="State"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please select your state!',
-                        },
-                    ]}>
-                    <Input />
-                </Form.Item>
-            )}
-
-            {/* Main address input */}
-            {user.userType === 'vendor' && (
-                <Form.Item
-                    name={['address', 'mainAddress']}
-                    label="Main Address"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please enter your address!',
-                        },
-                    ]}>
-                    <Input />
-                </Form.Item>
-            )}
-
-            {/* Phone number input */}
-            <Form.Item name="phone" label="Phone Number">
-                <Input.Group compact>
-                    <Form.Item
-                        name={['phone', 'countryCode']}
-                        noStyle
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Please enter your country code!',
-                            },
-                            {
-                                message: 'Wrong country code format',
-                                validator: (rule, val) => {
-                                    if (!val) return Promise.resolve(true)
-                                    return /\+\d{1,3}/.test(val)
-                                        ? Promise.resolve(true)
-                                        : Promise.reject('Country code format is incorrect!')
-                                },
-                            },
-                        ]}>
-                        <Input style={{ width: '20%' }} placeholder="+90" />
-                    </Form.Item>
-                    <Form.Item
-                        name={['phone', 'number']}
-                        noStyle
-                        rules={[
-                            { required: true, message: 'Please enter your phone number!' },
-                            {
-                                message: 'Wrong phone number format',
-                                validator: (rule, val) => {
-                                    if (!val) return Promise.resolve(true)
-                                    return /\d{4,}/.test(val)
-                                        ? Promise.resolve(true)
-                                        : Promise.reject('Phone number format is incorrect!')
-                                },
-                            },
-                        ]}>
-                        <InputNumber style={{ width: '80%' }} />
-                    </Form.Item>
-                </Input.Group>
             </Form.Item>
 
             <Form.Item {...tailFormItemLayout}>
