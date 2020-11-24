83b47685de2836d0deee84927a5af1eb7e105e68
Asli Aykan <asliayk@hotmail.com>
2020-11-24 11:22:14 +0300

    observer added

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/CartFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/CartFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.getflix.R
