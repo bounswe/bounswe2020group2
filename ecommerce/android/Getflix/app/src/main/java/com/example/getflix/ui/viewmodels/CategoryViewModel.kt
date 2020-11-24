04d46c2d4b96881b8d9401fb57c018ff2a7111b9
karalleyna <36455180+karalleyna@users.noreply.github.com>
2020-11-24 06:05:09 +0300

    Update type of mock data

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/viewmodels/CategoryViewModel.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/viewmodels/CategoryViewModel.kt
@@ -21,51 +21,65 @@ class CategoryViewModel(application: Application) : AndroidViewModel(application
     private val _displayedCategory = MutableLiveData<CategoryModel>()
     val displayedCategory: LiveData<CategoryModel>
         get() = _displayedCategory
-    
+
     fun setCategory(id: Int) {
         if (id == 1) {
-            var zaraJacket1 = ProductModel(1, "Jacket", 222, null, "Zara")
-            var zaraJacket2 = ProductModel(2, "Jacket", 231, null, "Zara")
-            var zaraJacket3 = ProductModel(3, "Jacket", 321, null, "Zara")
-            var jackets = mutableListOf<ProductModel>(zaraJacket1, zaraJacket2, zaraJacket3)
-            var subcategoryModel2 = SubcategoryModel("Jackets", mutableListOf(zaraJacket1, zaraJacket2, zaraJacket3))
-            var zaraSkirt1 = ProductModel(4, "Skirt", 79, null, "Zara")
-            var zaraSkirt2 = ProductModel(5, "Skirt", 93, null, "Zara")
-            var zaraSkirt3 = ProductModel(6, "Skirt", 102, null, "Zara")
-            var skirts = mutableListOf<ProductModel>(zaraSkirt1, zaraSkirt2, zaraSkirt3)
-            var zaraDress1 = ProductModel(7, "Dress", 60, null, "Zara")
-            var zaraDress2 = ProductModel(8, "Dress", 142, null, "Zara")
-            var zaraDress3 = ProductModel(9, "Dress", 201, null, "Zara")
-            var dresses = mutableListOf<ProductModel>(zaraSkirt1, zaraSkirt2, zaraSkirt3)
+            var zaraJacket1 =
+                PModel(1, "Jacket", 222, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var zaraJacket2 =
+                PModel(2, "Jacket", 231, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var zaraJacket3 =
+                PModel(3, "Jacket", 321, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var jackets = mutableListOf<PModel>(zaraJacket1, zaraJacket2, zaraJacket3)
+            var subcategoryModel2 =
+                SubcategoryModel("Jackets", mutableListOf(zaraJacket1, zaraJacket2, zaraJacket3))
+            var zaraSkirt1 = PModel(4, "Skirt", 79, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var zaraSkirt2 = PModel(5, "Skirt", 93, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var zaraSkirt3 = PModel(6, "Skirt", 102, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var skirts = mutableListOf<PModel>(zaraSkirt1, zaraSkirt2, zaraSkirt3)
+            var zaraDress1 = PModel(7, "Dress", 60, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var zaraDress2 = PModel(8, "Dress", 142, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var zaraDress3 = PModel(9, "Dress", 201, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var dresses = mutableListOf<PModel>(zaraSkirt1, zaraSkirt2, zaraSkirt3)
             var subcategoryModel1 = SubcategoryModel("Skirts", skirts)
             var subcategoryModel3 = SubcategoryModel("Dresses", dresses)
-            var subcategories = mutableListOf<SubcategoryModel>(subcategoryModel1, subcategoryModel3, subcategoryModel2)
+            var subcategories = mutableListOf<SubcategoryModel>(
+                subcategoryModel1,
+                subcategoryModel3,
+                subcategoryModel2
+            )
             var categoryModel = CategoryModel("Woman", subcategories)
         } else if (id == 2) {
-            var zaraJacket1 = ProductModel(10, "Jacket", 222, null, "Zara")
-            var zaraJacket2 = ProductModel(11, "Jacket", 231, null, "Zara")
-            var zaraJacket3 = ProductModel(12, "Jacket", 321, null, "Zara")
-            var jackets = mutableListOf<ProductModel>(zaraJacket1, zaraJacket2, zaraJacket3)
-            var subcategoryModel2 = SubcategoryModel("Jeans", mutableListOf(zaraJacket1, zaraJacket2, zaraJacket3))
-            var zaraJean1 = ProductModel(13, "Jean", 79, null, "Zara")
-            var zaraJean2 = ProductModel(14, "Jean", 93, null, "Zara")
-            var zaraJean3 = ProductModel(15, "Jean", 102, null, "Zara")
-            var jeans = mutableListOf<ProductModel>(zaraJean1, zaraJean2, zaraJean3)
-            var zaraShirt1 = ProductModel(16, "Shirt", 60, null, "Zara")
-            var zaraShirt2 = ProductModel(17, "Shirt", 142, null, "Zara")
-            var zaraShirt3 = ProductModel(18, "Shirt", 201, null, "Zara")
-            var shirts = mutableListOf<ProductModel>(zaraShirt1, zaraShirt2, zaraShirt3)
+            var zaraJacket1 =
+                PModel(10, "Jacket", 222, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var zaraJacket2 =
+                PModel(11, "Jacket", 231, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var zaraJacket3 =
+                PModel(12, "Jacket", 321, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var jackets = mutableListOf<PModel>(zaraJacket1, zaraJacket2, zaraJacket3)
+            var subcategoryModel2 =
+                SubcategoryModel("Jeans", mutableListOf(zaraJacket1, zaraJacket2, zaraJacket3))
+            var zaraJean1 = PModel(13, "Jean", 79, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var zaraJean2 = PModel(14, "Jean", 93, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var zaraJean3 = PModel(15, "Jean", 102, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var jeans = mutableListOf<PModel>(zaraJean1, zaraJean2, zaraJean3)
+            var zaraShirt1 = PModel(16, "Shirt", 60, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var zaraShirt2 = PModel(17, "Shirt", 142, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var zaraShirt3 = PModel(18, "Shirt", 201, "1", "Zara", 1, 1, 1, "1", "1", "1", "1", "1")
+            var shirts = mutableListOf<PModel>(zaraShirt1, zaraShirt2, zaraShirt3)
             var subcategoryModel1 = SubcategoryModel("Shirts", shirts)
             var subcategoryModel3 = SubcategoryModel("jeans", jeans)
-            var subcategories = mutableListOf<SubcategoryModel>(subcategoryModel2, subcategoryModel3, subcategoryModel1)
+            var subcategories = mutableListOf<SubcategoryModel>(
+                subcategoryModel2,
+                subcategoryModel3,
+                subcategoryModel1
+            )
             var categoryModel = CategoryModel("Man", subcategories)
         } else {
 
         }
 
     }
-
-
 
 
     private var job: Job? = null
@@ -85,11 +99,6 @@ class CategoryViewModel(application: Application) : AndroidViewModel(application
         }
 
     }  */
-
-
-
-
-
 
 
 }
