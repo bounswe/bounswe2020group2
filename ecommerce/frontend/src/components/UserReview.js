e4dfab53e8dd6d593692e40cb2e7ecea5aaa5fef
Abdullah Yildiz <abdullahyildiz321@gmail.com>
2020-12-06 21:20:00 +0300

    added backend call subpart

--- ecommerce/frontend/src/components/UserReview.js
+++ ecommerce/frontend/src/components/UserReview.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import { Button, Rate, Card } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Rate } from 'antd'
 import { Link } from 'react-router-dom'
 import './UserReview.less'
+import { api } from '../api'
 import { useAppContext } from '../context/AppContext'
+import { notification } from 'antd'
 
-export const UserReview = () => {
+export const UserReview = product_id => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [reviews, setReviews] = useState([])
+
+    // useEffect(() => {
+    //     async function fetch() {
+    //         try {
+    //             setIsLoading(true)
+    //             try {
+    //                 const { data } = await api.get(`/product/${product_id}/reviews`)
+    //                 setReviews(data.reviews)
+    //                 // const { id, user_id, vendor_id, rating, comment, created_at } = data
+    //             } catch (error) {
+    //                 notification.error({ description: 'Failed to fetch comments' })
+    //                 console.error(error)
+    //             } finally {
+    //             }
+    //         } finally {
+    //             setIsLoading(false)
+    //         }
+    //     }
+    // }, [])
+
     const Review = ({ review: { comment, author, date, rating } }) => {
         return (
             <div className="review-item-content">
@@ -25,7 +49,7 @@ export const UserReview = () => {
             </div>
         )
     }
-    const reviews = [
+    setReviews([
         {
             comment:
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
@@ -39,7 +63,7 @@ export const UserReview = () => {
             date: 'December 11',
             rating: 3,
         },
-    ]
+    ])
 
     return (
         <div className="review-items">
