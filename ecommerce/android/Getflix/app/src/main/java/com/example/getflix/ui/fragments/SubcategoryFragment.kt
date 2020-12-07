2fa6900a08de0871933f188f4213ddd996c179d7
hande <hansirikci@gmail.com>
2020-12-07 23:31:28 +0300

    adapter and mock data added to subcategories

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/SubcategoryFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/SubcategoryFragment.kt
@@ -6,16 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.example.getflix.R
 import com.example.getflix.databinding.FragmentSubcategoryBinding
+import com.example.getflix.models.ProductModel
+import com.example.getflix.ui.adapters.FavoritesAdapter
+import com.example.getflix.ui.viewmodels.FavoritesViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 
 class SubcategoryFragment : Fragment() {
+
+    private lateinit var viewModel: FavoritesViewModel
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -25,16 +33,32 @@ class SubcategoryFragment : Fragment() {
                 container, false
         )
 
-        activity?.toolbar!!.visibility = View.GONE
-        val navController =
-            Navigation.findNavController(requireActivity(), R.id.my_nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
-        binding.toolbar.title = ""
-        val args = SubcategoryFragmentArgs.fromBundle(requireArguments())
-        val subCat = args.subName
-        binding.toolbarTitle.text = subCat
+        viewModel = ViewModelProvider(this).get(FavoritesViewModel::class.java)
+        binding.viewmodel = FavoritesViewModel()
+        val recView = binding?.productList as RecyclerView
+        val products = arrayListOf( ProductModel(1,"Bag","100 TL",null,"Vıntage Bag"),
+                ProductModel(1,"iPhone 7","4815 TL",null,"Best Phone"),
+                ProductModel(1,"Pullover","36 TL",null,"Black Pullover"),
+                ProductModel(1,"Notebook","32 TL",null,"Spiral Notebook"),
+                ProductModel(1,"Pencil","13 TL",null,"Black Pencil"),
+                ProductModel(1,"Skirt","30 TL",null,"Vıntage Skirt"),
+                ProductModel(1,"T-Shirt","23 TL",null,"Vıntage T-Shirt"),
+                ProductModel(1,"Book","20 TL",null,"Bestseller Book")   ,
+                ProductModel(1,"T-Shirt","23 TL",null,"Black T-Shirt"),
+                ProductModel(1,"Book","20 TL",null,"Bestseller Book"))
+        val productListAdapter = FavoritesAdapter(products)
+        recView.adapter = productListAdapter
+        recView.setHasFixedSize(true)
 
+        for(product in products) {
+            viewModel.addProduct(product)
+        }
+
+        viewModel.productList.observe(viewLifecycleOwner, Observer{
+            it?.let {
+                productListAdapter.submitList(it)
+            }
+        })
 
         return binding.root
     }
