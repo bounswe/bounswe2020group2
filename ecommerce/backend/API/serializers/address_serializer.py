d353b0e3df12bb4a113f6edbf3b62e249e36e576
omerfdeniz <omerfaruk.deniz@gmail.com>
2020-12-08 21:50:13 +0300

    Add files via upload

---
+++ ecommerce/backend/API/serializers/address_serializer.py
@@ -0,0 +1,14 @@
+from API.models.user import Customer
+from rest_framework import serializers
+from ..models import Customer, Address
+
+class AddressSerializer(serializers.ModelSerializer):
+    first_name = serializers.SerializerMethodField('get_first_name')
+    class Meta:
+        model = Address
+        fields = ('id', 'title', 'phone_number', 'first_name, last_name', 'address', 'province', 'city', 'country')
+
+    def get_first_name(self, obj):
+        return Customer.objects.filter(user_id=obj.user.id).first().first_name
+    def get_last_name(self, obj):
+        return Customer.objects.filter(user_id=obj.user.id).first().last_name
