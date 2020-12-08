35fd746fea20e2afc17f013b43aeced6bc28dec7
karalleyna <36455180+karalleyna@users.noreply.github.com>
2020-12-08 12:58:51 +0300

    [ANDROID] Register View Model & Request for sign up endpoint (#224)
    - Create the view model RegisterFragment
    - Handle repeated conditions and add live data observers
    - Create the data model for sign up body
    - Create the data model for the sign up response
    - Add signup request

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/services/GetflixApiService.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/services/GetflixApiService.kt
@@ -1,8 +1,6 @@
 package com.example.getflix.services
 
-import com.example.getflix.models.LoginRequest
-import com.example.getflix.models.LoginResponse
-import com.example.getflix.models.PModel
+import com.example.getflix.models.*
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Call
@@ -46,6 +44,11 @@ interface GetflixApiService {
     @Headers("Content-Type: application/json")
     @POST("regularlogin")
     fun getUserInformation(@Body userData: LoginRequest): Call<LoginResponse>
+
+
+    @Headers("Content-Type: application/json")
+    @POST("regularsignup")
+    fun signUp(@Body signUpCredentials: SignUpCredentials): Call<SignUpResponse>
 
     @GET("products/homepage/{numberOfProducts}")
     suspend fun getProducts(@Path("numberOfProducts") numberOfProducts: Int): Response<List<PModel>>
