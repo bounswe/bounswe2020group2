0ac32a688a2cb0c50fb84fe0cb228a841e4c8207
Mehdi Saffar <saffar.mehdi3@gmail.com>
2020-12-09 22:48:44 +0300

    add draft code for add and remove product from list

--- ecommerce/frontend/src/components/ChooseListModalInner.js
+++ ecommerce/frontend/src/components/ChooseListModalInner.js
@@ -1,49 +1,79 @@
 import './ChooseListModalInner.less'
 
 import React, { useEffect, useState } from 'react'
-import { Button, List } from 'antd'
+import { Button, List, notification } from 'antd'
 import { sleep } from '../utils'
 import { HeartOutlined, HeartFilled } from '@ant-design/icons'
 
-export const ChooseListModalInner = ({ onChooseList }) => {
+export const ChooseListModalInner = ({ product, onChooseList }) => {
     const [lists, setLists] = useState([])
     const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        async function fetch() {
-            try {
-                setLoading(true)
-                await sleep(1000)
-                // backend call to get lists of user
+    const getLists = async (product) => {
+        try {
+            setLoading(true)
+            await sleep(1000)
+            // backend call to get lists of user
 
-                setLists([
-                    {
-                        id: 1,
-                        name: 'Electronics',
-                        hasProduct: true,
-                    },
+            setLists([
+                {
+                    id: 1,
+                    name: 'Electronics',
+                    hasProduct: true,
+                },
 
-                    {
-                        id: 2,
-                        name: 'Thanksgiving',
-                        hasProduct: false,
-                    },
-                    {
-                        id: 3,
-                        name: 'Blackfriday',
-                        hasProduct: true,
-                    },
-                ])
-            } catch (error) {
-            } finally {
-                setLoading(false)
-            }
+                {
+                    id: 2,
+                    name: 'Thanksgiving',
+                    hasProduct: false,
+                },
+                {
+                    id: 3,
+                    name: 'Blackfriday',
+                    hasProduct: true,
+                },
+            ])
+        } catch (error) {
+        } finally {
+            setLoading(false)
         }
+    }
 
-        fetch()
+    useEffect(() => {
+        getLists(product)
     }, [])
 
-    const onListClick = list => () => onChooseList(list)
+    const removeProductFromList = async (product, list) => {
+        try {
+            await sleep(1000) // actual code here
+            notification.success({description: "Added product to list"})
+        } catch (error) {
+            notification.error({description: "Failed to remove product from list"})
+
+        } finally {
+
+        }
+    }
+
+    const addProductToList = async (product, list) => {
+        try {
+            await sleep(1000) // actual code here
+            notification.success({description: "Added product to list"})
+        } catch (error) {
+            notification.error({description: "Failed to remove product from list"})
+        } finally {
+
+        }
+    }
+
+    const onListClick = list => async () => {
+        if (list.hasProduct) {
+            await removeProductFromList(list)
+        } else {
+            await addProductToList(list)
+        }
+        await getLists(product)
+    }
 
     return (
         <List loading={loading} size="large" itemLayout="horizontal">
