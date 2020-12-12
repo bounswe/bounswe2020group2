2d3975f742b57522746e6d346937974cca0c9b08
Furkan Nane <furkannane6@gmail.com>
2020-12-12 12:33:09 +0300

    test update again

--- ecommerce/backend/API/tests/test_product_detail.py
+++ ecommerce/backend/API/tests/test_product_detail.py
@@ -18,7 +18,7 @@ class ProductDetail(TestCase):
         v = Vendor.objects.create(id=1, user = u, first_name="omerfaruk", last_name="deniz",rating_count=10,total_rating=3)
         Product.objects.create(id=product_id_for_test, name = "Mavi T-shirt", price = 100, 
             creation_date = "2019-08-20T07:22:34Z",total_rating = 20, discount=0.1,
-            rating_count = 5, stock_amount = 10, short_description = "yaza özel",long_description = "i love omer faruk deniz", subcategory = s, brand = b, vendor = v)
+            rating_count = 5, stock_amount = 10, short_description = "yaza özel",long_description = "gerçekten yaza özel yav", subcategory = s, brand = b, vendor = v)
 
     def test_product_detail(self):
         response = client.get(reverse(product_detail, args = [product_id_for_test]))
