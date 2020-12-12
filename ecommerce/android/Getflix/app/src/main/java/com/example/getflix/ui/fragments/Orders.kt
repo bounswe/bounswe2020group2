4f74d69628c1b56f4fe4d4945b29126f91364c4c
hande <hansirikci@gmail.com>
2020-12-12 16:05:59 +0300

    navigations added

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/Orders.kt
+++
@@ -1,4 +0,0 @@
-package com.example.getflix.ui.fragments
-
-class Orders {
-}
--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/ProfileFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/ProfileFragment.kt
@@ -10,7 +10,11 @@ import androidx.navigation.findNavController
 import com.example.getflix.R
 import com.example.getflix.activities.MainActivity
 import com.example.getflix.databinding.FragmentProfileBinding
+import com.example.getflix.ui.fragments.ProfileFragmentDirections.Companion.actionProfileFragmentToAdddressFragment
+import com.example.getflix.ui.fragments.ProfileFragmentDirections.Companion.actionProfileFragmentToBankAccountFragment
 import com.example.getflix.ui.fragments.ProfileFragmentDirections.Companion.actionProfileFragmentToLoginFragment
+import com.example.getflix.ui.fragments.ProfileFragmentDirections.Companion.actionProfileFragmentToOrderInfoFragment
+import com.example.getflix.ui.fragments.ProfileFragmentDirections.Companion.actionProfileFragmentToUserInfoFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
@@ -50,6 +54,15 @@ class ProfileFragment : Fragment() {
                  transaction.replace(R.id.my_nav_host_fragment, LoginFragment())
                  transaction.disallowAddToBackStack()
                  transaction.commit() */
+
+            binding.ordersButton.setOnClickListener {
+                view?.findNavController()?.navigate(actionProfileFragmentToOrderInfoFragment())}
+            binding.userInfoButton.setOnClickListener {
+                view?.findNavController()?.navigate(actionProfileFragmentToUserInfoFragment())}
+            binding.addressinfo.setOnClickListener {
+                view?.findNavController()?.navigate(actionProfileFragmentToAdddressFragment())}
+            binding.bankAccountInfoButton.setOnClickListener {
+                view?.findNavController()?.navigate(actionProfileFragmentToBankAccountFragment())}
 
          }
 
