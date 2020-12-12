b90266bf768b92cdc07508193cff0a3534036c8c
umutoksuz <44121303+umutoksuz@users.noreply.github.com>
2020-12-12 16:23:38 +0300

    Deploying Backend (#237)
    * [BACKEND] Review (Comment & Rating) Endpoints (#236)
    
    Review functionalities added

---
+++ ecommerce/backend/API/tests/test_review.py
@@ -0,0 +1,137 @@
+from rest_framework.test import APIClient
+from django.test import TestCase
+from django.urls import reverse
+from ..models import User, Vendor, Product, Review, Purchase, Category, Subcategory, Brand, Address
+from ..utils import OrderStatus
+import datetime
+
+class ReviewTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        body = {
+            'username': 'testuser',
+            'email': 'test@mail.com',
+            'password': '12345678',
+            'firstname': 'test',
+            'lastname': 'user'
+        }
+        response = self.client.post(reverse('register'), body, 'json')
+        u = User.objects.filter(username='testuser').first()
+        self.uid = u.id
+        c = Category.objects.create(id=4, name="Clothing")
+        s = Subcategory.objects.create(id=10, category = c, name="Men's Fashion")
+        a = Address.objects.create(id=1, user=u, address="istanbul")
+        b = Brand.objects.create(id=1, name="Mavi")
+        v1 = Vendor.objects.create(id=1, user = u, first_name="omerfaruk", last_name="deniz")
+        v2 = Vendor.objects.create(id=2, user = u, first_name="umut", last_name="öksüz")
+        p1 = Product.objects.create(id=1, name = "Mavi T-shirt", price = 100, 
+            creation_date = "2019-08-20T07:22:34Z", total_rating = 4, 
+            rating_count = 20, stock_amount = 10, short_description = "yaza özel", subcategory = s, brand = b, vendor = v1)
+        p2 = Product.objects.create(id=2, name = "Mavi Pantolon", price = 100, 
+            creation_date = "2019-08-20T07:22:34Z", total_rating = 4, 
+            rating_count = 20, stock_amount = 10, short_description = "yaza özel", subcategory = s, brand = b, vendor = v1)
+        Purchase.objects.create(user=u, product=p1, amount = 1, unit_price = 100, name="Mavi T-shirt", 
+        status = OrderStatus.DELIVERED.value, address=a, purchase_date=datetime.date.today())
+        body = {
+            'username': 'testuser',
+            'password': '12345678'
+        }
+        response = self.client.post(reverse('login'), body, 'json')
+        token = response.data["user"]["token"]
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+    
+    def test_add_review(self):
+        comment = {
+            'user_id': self.uid,
+            'product_id': 1,
+            'vendor_id': None,
+            'rating': 5,
+            'comment': "Awesome!"
+        }
+        response = self.client.post(reverse('review'), comment, 'json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["status"]["successful"], True)
+
+    def test_add_review_not_purchased(self):
+        comment = {
+            'user_id': self.uid,
+            'product_id': 2,
+            'vendor_id': None,
+            'rating': 5,
+            'comment': "Awesome!"
+        }
+        response = self.client.post(reverse('review'), comment, 'json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["status"]["successful"], False)
+        self.assertEqual(response.data["status"]["message"], "You cannot review a product that you have not bought.")
+
+    def test_add_multiple_review(self):
+        comment = {
+            'user_id': self.uid,
+            'product_id': 1,
+            'vendor_id': None,
+            'rating': 5,
+            'comment': "Awesome!"
+        }
+        response = self.client.post(reverse('review'), comment, 'json')
+        response = self.client.post(reverse('review'), comment, 'json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["status"]["successful"], False)
+        self.assertEqual(response.data["status"]["message"], "You have already reviewed this product.")
+
+    def test_review_vendor(self):
+        comment = {
+            'user_id': self.uid,
+            'product_id': None,
+            'vendor_id': 1,
+            'rating': 5,
+            'comment': "Awesome vendor!"
+        }
+        response = self.client.post(reverse('review'), comment, 'json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["status"]["successful"], True)
+
+    def test_review_vendor_not_purchased(self):
+        comment = {
+            'user_id': self.uid,
+            'product_id': None,
+            'vendor_id': 2,
+            'rating': 5,
+            'comment': "Awesome vendor!"
+        }
+        response = self.client.post(reverse('review'), comment, 'json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["status"]["successful"], False)
+        self.assertEqual(response.data["status"]["message"], "You cannot review a vendor that you have not purchased a prouct from.")
+
+    def test_add_multiple_review_vendor(self):
+        comment = {
+            'user_id': self.uid,
+            'product_id': None,
+            'vendor_id': 1,
+            'rating': 5,
+            'comment': "Awesome vendor!"
+        }
+        response = self.client.post(reverse('review'), comment, 'json')
+        response = self.client.post(reverse('review'), comment, 'json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["status"]["successful"], False)
+        self.assertEqual(response.data["status"]["message"], "You have already reviewed this vendor.")
+
+    def test_delete_review(self):
+        comment = {
+            'user_id': self.uid,
+            'product_id': 1,
+            'vendor_id': None,
+            'rating': 5,
+            'comment': "Awesome!"
+        }
+        response = self.client.post(reverse('review'), comment, 'json')
+        review_id = response.data["review"]["id"]
+        body = { 'id': review_id }
+        response = self.client.delete(reverse('review'), body, 'json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["status"]["successful"], True)
+        self.assertEqual(response.data["status"]["message"], "Review is deleted successfully.")
