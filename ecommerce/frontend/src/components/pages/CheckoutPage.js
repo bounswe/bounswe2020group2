6543227afd64a392bcec3aed087c13147b10c578
Özdeniz Dolu <ozdendolu@windowslive.com>
2020-12-17 19:02:27 +0300

    Address/card list have been put in a container

--- ecommerce/frontend/src/components/pages/CheckoutPage.js
+++ ecommerce/frontend/src/components/pages/CheckoutPage.js
@@ -1,9 +1,9 @@
 import { CreditCardList } from '../cardlist/CreditCardList'
 import { AddressList } from '../addresslist/AddressList'
-
+import "./CheckoutPage.less"
 export const CheckoutPage = () => {
     return (
-        <div>
+        <div className="payment-options-container">
             <CreditCardList />
             <AddressList />
         </div>
