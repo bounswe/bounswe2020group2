bf40c6c0d115c0ba6d38df6eb48925a4f4740d65
Burak Çetin <burakcetin435@gmail.com>
2020-12-18 20:51:38 +0300

    B search 2 (#255)
    * [BACKEND] implement search/products

--- ecommerce/backend/API/views/__init__.py
+++ ecommerce/backend/API/views/__init__.py
@@ -1,7 +1,7 @@
 from .account import *
 from .product import *
 from .shopping_cart import * 
-from .address import *
+from .search import * 
 from .checkout import *
 from .review import *
-
+from .address import *
