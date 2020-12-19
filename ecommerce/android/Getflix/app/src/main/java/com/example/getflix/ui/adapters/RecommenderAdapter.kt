6d3dede474ff11f84a9cb2d757972df93d541163
karalleyna <36455180+karalleyna@users.noreply.github.com>
2020-12-19 20:23:43 +0300

    Create RecommenderAdapter

---
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/adapters/RecommenderAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.getflix.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.getflix.databinding.CardRecommendedProductBinding
+import com.example.getflix.databinding.FragmentProductBinding
+import com.example.getflix.models.ProductModel
+
+class RecommenderAdapter :
+        ListAdapter<ProductModel, RecommenderAdapter.ViewHolder>(RecommenderDiffCallback()) {
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
+    private fun ViewHolder.bind(product: ProductModel) {
+        binding.product = product
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    class ViewHolder private constructor(val binding: CardRecommendedProductBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = CardRecommendedProductBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+
+    }
+}
+
+class RecommenderDiffCallback : DiffUtil.ItemCallback<ProductModel>() {
+    override fun areItemsTheSame(oldItem: ProductModel, newItem: ProductModel): Boolean {
+        return oldItem === newItem
+    }
+
+    override fun areContentsTheSame(oldItem: ProductModel, newItem: ProductModel): Boolean {
+        return oldItem == newItem
+    }
+
+}
+
