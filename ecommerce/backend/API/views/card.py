493d9b1d82f9e307401fc31bfeb234a09a4b4656
omerfdeniz <omerfaruk.deniz@gmail.com>
2020-12-20 22:51:28 +0300

    Update card.py

--- ecommerce/backend/API/views/card.py
+++ ecommerce/backend/API/views/card.py
@@ -11,8 +11,8 @@ from ..serializers.card_serializer import *
 @permission_classes([permissions.AllowAnonymous])
 def manage_specific_card(request, customer_id, card_id):
     # reaching others' content is forbidden
-    #if request.user.pk != customer_id:
-    #    return Response(status=status.HTTP_403_FORBIDDEN)
+    if request.user.pk != customer_id:
+        return Response(status=status.HTTP_403_FORBIDDEN)
     # no such user exists
     if User.objects.filter(id=customer_id).first() is None:
         return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -57,8 +57,8 @@ def manage_specific_card(request, customer_id, card_id):
 @permission_classes([permissions.AllowAnonymous])
 def manage_cards(request, customer_id):
     # reaching others' content is forbidden
-    #if request.user.pk != customer_id:
-    #    return Response(status=status.HTTP_403_FORBIDDEN)
+    if request.user.pk != customer_id:
+        return Response(status=status.HTTP_403_FORBIDDEN)
     # no such user exists
     if User.objects.filter(id=customer_id).first() is None:
         return Response(status=status.HTTP_400_BAD_REQUEST)
