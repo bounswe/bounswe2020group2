cc67f05a459f0ffd9fe9664a3e4f6bbe754d3809
hande <hansirikci@gmail.com>
2020-12-20 15:04:18 +0300

    address view model created

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/viewmodels/AddressViewModel.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/viewmodels/AddressViewModel.kt
@@ -1,4 +1,25 @@
 package com.example.getflix.ui.viewmodels
 
-class AddressViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.getflix.models.AddressModel
+
+class AddressViewModel  : ViewModel() {
+
+    private val _addressList = MutableLiveData<MutableList<AddressModel>>()
+    val productList: LiveData<MutableList<AddressModel>>
+        get() = _addressList
+
+    fun addProduct(addressModel: AddressModel) {
+        if (_addressList.value != null) {
+            val addresses = _addressList.value
+            addresses?.add(addressModel)
+            _addressList.value = addresses
+        } else {
+            val addresses = arrayListOf<AddressModel>()
+            addresses.add(addressModel)
+            _addressList.value = addresses
+        }
+    }
 }
