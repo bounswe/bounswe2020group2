cb811d50f08d6c864151ce436e360ceab0ee69fe
omerfdeniz <omerfaruk.deniz@gmail.com>
2020-12-20 13:42:45 +0300

    Add files via upload

--- ecommerce/backend/API/views/address.py
+++ ecommerce/backend/API/views/address.py
@@ -8,34 +8,60 @@ from ..utils import permissions, Role
 from ..models import User, Address
 from ..serializers.address_serializer import *
 
-@api_view(['GET', 'DELETE'])
+@api_view(['PUT', 'GET', 'DELETE'])
 @permission_classes([permissions.AllowAnonymous])
 def manage_single_address(request, customer_id, address_id):
+    # reaching others' content is forbidden
+    #if request.user.pk != customer_id:
+    #    return Response(status=status.HTTP_403_FORBIDDEN)
+    # no such user exists
     if User.objects.filter(id=customer_id).first() is None:
         return Response(status=status.HTTP_400_BAD_REQUEST)
     #get single address
     if request.method == 'GET':
-        address = Address.objects.filter(user_id=customer_id).filter(id=address_id)
+        address = Address.objects.filter(user_id=customer_id).filter(id=address_id).first()
         if address is None:
             return Response({'successful': False, 'message': "No such address is found"})
-        address_serializer = AddressResponseSerializer(address, many=True)
+        address_serializer = AddressResponseSerializer(address)
         return Response(address_serializer.data)
     #delete single address 
     elif request.method == 'DELETE':
         try:
             address = Address.objects.filter(user_id=customer_id).filter(id=address_id)
-            if address is not None:
-                address.delete()
+            if address is not None or not address.is_deleted:
+                address.is_deleted = True
                 return Response({'successful': True, 'message': "Successfully deleted"})
             else:
                 return Response({'successful': True, 'message': "No such address is found"})
         except Exception as e:
             return Response({'successful': False, 'message': str(e)})
+    #update an address
+    elif request.method == 'PUT':
+        address = Address.objects.filter(user_id=customer_id).filter(id=address_id).first()
+        if address is None or address.is_deleted:
+            return Response({'successful': False, 'message': "No such address is found"})
+        address.title = request.data.get("title")
+        address.name = request.data.get("name")
+        address.surname = request.data.get("surname")
+        address.address = request.data.get("address")
+        address.province = request.data.get("province")
+        address.city = request.data.get("city")
+        phone = request.data.get("phone")
+        address.phone_number = phone.get("number")
+        address.phone_country_code = phone.get("country_code")
+        address.country = request.data.get("title")
+        address.zip_code = request.data.get("zip_code")
+        address.save()
+        return Response({'successful': True, 'message': "Address is successfully updated"})
     return Response({'successful': False, 'message': "Error occurred"})
 
 @api_view(['GET', 'POST'])
 @permission_classes([permissions.AllowAnonymous])
 def manage_multiple_addresses(request, customer_id):
+    # reaching others' content is forbidden
+    #if request.user.pk != customer_id:
+    #    return Response(status=status.HTTP_403_FORBIDDEN)
+    # no such user exists
     if User.objects.filter(id=customer_id).first() is None:
         return Response(status=status.HTTP_400_BAD_REQUEST)
     user = User.objects.get(pk=int(customer_id))
@@ -49,6 +75,8 @@ def manage_multiple_addresses(request, customer_id):
         serializer = AddressRequestSerializer(data=request.data)
         if serializer.is_valid():
             title = serializer.validated_data.get("title")
+            name = serializer.validated_data.get("name")
+            surname = serializer.validated_data.get("surname")
             address = serializer.validated_data.get("address")
             province = serializer.validated_data.get("province")
             city = serializer.validated_data.get("city")
@@ -57,7 +85,7 @@ def manage_multiple_addresses(request, customer_id):
             phone_country_code = phone.get("country_code")
             phone_number = phone.get("number")
             zip_code = serializer.validated_data.get("zip_code")
-            address = Address(user=user, title=title, address=address, province=province, city=city, 
+            address = Address(user=user, name=name, surname=surname, title=title, address=address, province=province, city=city, 
                 country=country, phone_country_code=phone_country_code, phone_number=phone_number, zip_code=zip_code)
             address.save()
             return Response({'successful': True, 'message': "Address is successfully added"})
---
+++ ecommerce/backend/API/views/card.py
@@ -0,0 +1,87 @@
+from rest_framework import exceptions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status
+
+from ..utils import permissions, Role
+from ..models import User, Card
+from ..serializers.card_serializer import *
+
+@api_view(['PUT', 'GET', 'DELETE'])
+@permission_classes([permissions.AllowAnonymous])
+def manage_single_card(request, customer_id, card_id):
+    # reaching others' content is forbidden
+    #if request.user.pk != customer_id:
+    #    return Response(status=status.HTTP_403_FORBIDDEN)
+    # no such user exists
+    if User.objects.filter(id=customer_id).first() is None:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.get(pk=int(customer_id))
+    #get single card
+    if request.method == 'GET':
+        card = Card.objects.filter(user_id=customer_id).filter(id=card_id).first()
+        if card is None or card.is_deleted:
+            return Response({'successful': False, 'message': "No such card is found"})
+        card_serializer = CardResponseSerializer(card)
+        return Response(card_serializer.data)
+    #delete single card 
+    elif request.method == 'DELETE':
+        try:
+            card = Card.objects.filter(user_id=customer_id).filter(id=card_id)
+            if card is not None or not card.is_deleted:
+                card.is_deleted = True # soft delete
+                card.save()
+                return Response({'successful': True, 'message': "Successfully deleted"})
+            else:
+                return Response({'successful': True, 'message': "No such card is found"})
+        except Exception as e:
+            return Response({'successful': False, 'message': str(e)})
+    #update a card
+    elif request.method == 'PUT':
+        card = Card.objects.filter(user_id=customer_id).filter(id=card_id).first()
+        if card is None or card.is_deleted:
+            return Response({'successful': False, 'message': "No such card is found"})
+        card.name = request.data.get("name")
+        card.owner_name = request.data.get("owner_name")
+        card.serial_number = request.data.get("serial_number")
+        expiration_date = request.data.get("expiration_date")
+        card.expiration_month = expiration_date.get("month")
+        card.expiration_year = expiration_date.get("year")
+        card.cvv = request.data.get("cvv")
+        card.save()
+        return Response({'successful': True, 'message': "Card is successfully updated"})
+    return Response({'successful': False, 'message': "Error occurred"})
+
+@api_view(['GET', 'POST'])
+@permission_classes([permissions.AllowAnonymous])
+def manage_multiple_cards(request, customer_id):
+    # reaching others' content is forbidden
+    #if request.user.pk != customer_id:
+    #    return Response(status=status.HTTP_403_FORBIDDEN)
+    # no such user exists
+    if User.objects.filter(id=customer_id).first() is None:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    user = User.objects.get(pk=int(customer_id))
+    # get all adresses
+    if request.method == 'GET':
+        cards = Card.objects.filter(user_id=customer_id)
+        card_serializer = CardResponseSerializer(cards, many=True)
+        return Response(card_serializer.data)
+    # add address
+    elif request.method == 'POST':
+        serializer = CardRequestSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            owner_name = serializer.validated_data.get("owner_name")
+            serial_number = serializer.validated_data.get("serial_number")
+            expiration_date = serializer.validated_data.get("expiration_date")
+            expiration_month = expiration_date.get("month")
+            expiration_year = expiration_date.get("year")
+            cvv = serializer.validated_data.get("cvv")
+            card = Card(user=user, name=name, owner_name=owner_name, serial_number=serial_number, expiration_month=expiration_month, expiration_year=expiration_year, cvv=cvv)
+            card.save()
+            return Response({'successful': True, 'message': "Card is successfully added"})
+    # add address
+    
+    return Response({'successful': False, 'message': "Error occurred"})
