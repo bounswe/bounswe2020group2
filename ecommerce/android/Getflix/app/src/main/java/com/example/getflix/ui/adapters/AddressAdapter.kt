b27a0f5cd4c2534f71e2e7d1edf6dcc93fb6452d
hande <hansirikci@gmail.com>
2020-12-20 15:03:31 +0300

    address adapter created

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/adapters/AddressAdapter.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/adapters/AddressAdapter.kt
@@ -1,2 +1,59 @@
 package com.example.getflix.ui.adapters
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ListView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.getflix.databinding.AddressCardItemBinding
+import com.example.getflix.databinding.ProductCardBinding
+import com.example.getflix.models.AddressModel
+import com.example.getflix.models.ProductModel
+
+
+class AddressAdapter(
+    private val addressList: ArrayList<AddressModel>?,
+) : ListAdapter<AddressModel, AddressAdapter.RowHolder>(CartDiffCallback()) {
+
+
+    class RowHolder(val binding: AddressCardItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(address: AddressModel, position: Int) {
+            binding.title = address.title
+            binding.addressInfo=address.address
+            binding.city=address.province
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): RowHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = AddressCardItemBinding.inflate(layoutInflater, parent, false)
+                return RowHolder(binding)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddressAdapter.RowHolder {
+        return RowHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: RowHolder, position: Int) {
+        addressList?.get(position)?.let { holder.bind(it, position) }
+    }
+
+
+}
+
+class CartDiffCallback : DiffUtil.ItemCallback<ProductModel>() {
+
+    override fun areItemsTheSame(oldItem: ProductModel, newItem: ProductModel): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: ProductModel, newItem: ProductModel): Boolean {
+        return oldItem == newItem
+    }
+
+}
