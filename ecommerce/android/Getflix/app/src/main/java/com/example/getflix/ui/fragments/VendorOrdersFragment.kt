8b1156abe8dd5a0f40cf39937712f9e6782a5acd
Asli Aykan <asliayk@hotmail.com>
2020-12-21 22:11:06 +0300

    vendor profile added

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/VendorOrdersFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/VendorOrdersFragment.kt
@@ -11,6 +11,8 @@ import com.example.getflix.R
 import com.example.getflix.databinding.FragmentVendorOrdersBinding
 import com.example.getflix.ui.viewmodels.VendorOrdersViewModel
 import com.example.getflix.ui.viewmodels.VendorProfileViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 
 class VendorOrdersFragment : Fragment() {
@@ -21,6 +23,7 @@ class VendorOrdersFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
+        activity?.toolbar!!.toolbar_title.text = getString(R.string.orders_vendor)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_vendor_orders,
                 container, false)
         viewModel = ViewModelProvider(this).get(VendorOrdersViewModel::class.java)
