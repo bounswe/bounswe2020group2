e534c81b32ad96d0bc5be897d35b43d46a2d9c60
karalleyna <36455180+karalleyna@users.noreply.github.com>
2020-12-21 10:01:05 +0300

    Create HomeCatagoriesAdapters

---
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/adapters/HomeCategoriesAdapter.kt
@@ -0,0 +1,69 @@
+package com.example.getflix.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.getflix.databinding.CardCategoryBinding
+import com.example.getflix.databinding.ListHorizontalsubsBinding
+import com.example.getflix.getCategoryImage
+import com.example.getflix.models.CategoryModel
+
+class HomeCategoriesAdapter :
+        ListAdapter<CategoryModel, HomeCategoriesAdapter.ViewHolder>(CategoryDiffCallback()) {
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
+    private fun ViewHolder.bind(category: CategoryModel) {
+        binding.categoryImage.setImageResource(getCategoryImage(category.name))
+        binding.categoryName.text = category.name
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun getItemViewType(position: Int) = position % 2
+
+    class ViewHolder private constructor(val binding: CardCategoryBinding, val viewType: Int) :
+            RecyclerView.ViewHolder(binding.root) {
+        companion object {
+
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = CardCategoryBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+
+}
+
+class CategoryDiffCallback : DiffUtil.ItemCallback<CategoryModel>() {
+    override fun areItemsTheSame(oldItem: CategoryModel, newItem: CategoryModel): Boolean {
+        return oldItem === newItem
+    }
+
+    override fun areContentsTheSame(oldItem: CategoryModel, newItem: CategoryModel): Boolean {
+        return oldItem == newItem
+    }
+
+}
+
+class ViewHolder1 private constructor(val binding: CardCategoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+    companion object {
+        fun from(parent: ViewGroup): HomeCategoriesAdapter.ViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = CardCategoryBinding.inflate(layoutInflater, parent, false)
+            return ViewHolder1(binding)
+        }
+    }
+
+}
+
