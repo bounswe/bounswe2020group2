dd4c2ff8efd33208eb8b864d401adc36549371bb
Hande Sirikci <hande.sirikci@boun.edu.tr>
2020-12-22 17:54:34 +0300

    [ANDROID] Creating Address Fragment (#272)
    * added necessary files
    * implementation added to use fab
    * address adapter created
    * address fragment and mock data  created
    * address view model created
    * address card design and floating action button added
    * fab added
    * refactor
    * fab action added
    * fab view bug fixed
    * navigation added
    * add address fragment added
    * nav bugs solved
    * required strings added
    * address list updated
    * address form added
    * card view style added

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/AddAddressFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/AddAddressFragment.kt
@@ -5,10 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import com.example.getflix.R
+import androidx.databinding.DataBindingUtil
 import com.example.getflix.databinding.FragmentAddAddressBinding
-import com.example.getflix.databinding.FragmentPaymentBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
@@ -21,10 +20,12 @@ class AddAddressFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         activity?.toolbar!!.toolbar_title.text = getString(R.string.address_add)
+
         val binding = DataBindingUtil.inflate<FragmentAddAddressBinding>(inflater, R.layout.fragment_add_address,
             container, false)
 
         return binding.root
+
     }
 
 
