21076a5c45c5cc34007f6f2cde7fac6f17e1e74e
Asli Aykan <asliayk@hotmail.com>
2020-12-22 21:55:25 +0300

    back button bug fixed

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/AddressFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/AddressFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.getflix.R
 import com.example.getflix.databinding.FragmentAddressBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
+import com.example.getflix.ui.fragments.AddressFragmentDirections.Companion.actionAddressFragmentToProfileFragment
 
 class AddressFragment : Fragment() {
 
@@ -22,6 +25,19 @@ class AddressFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentAddressBinding>(
                 inflater, R.layout.fragment_address,
                 container, false
+        )
+
+        activity?.onBackPressedDispatcher!!.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (isEnabled) {
+                        isEnabled = false
+                        view?.findNavController()!!
+                            .navigate(actionAddressFragmentToProfileFragment())
+                    }
+                }
+            }
         )
 
         return binding.root
--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/BankAccountFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/BankAccountFragment.kt
@@ -5,13 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.findNavController
 import com.example.getflix.R
 import com.example.getflix.databinding.FragmentBankAccountBinding
-import com.example.getflix.databinding.FragmentFavoritesBinding
+import com.example.getflix.ui.fragments.BankAccountFragmentDirections.Companion.actionBankAccountFragmentToProfileFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
@@ -26,6 +25,19 @@ class BankAccountFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentBankAccountBinding>(
                 inflater, R.layout.fragment_bank_account,
                 container, false
+        )
+
+        activity?.onBackPressedDispatcher!!.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (isEnabled) {
+                        isEnabled = false
+                        view?.findNavController()!!
+                            .navigate(actionBankAccountFragmentToProfileFragment())
+                    }
+                }
+            }
         )
 
         return binding.root
