bab7f0e06d9249ae634ca63ce6426d85451586d6
Hande Sirikci <hande.sirikci@boun.edu.tr>
2020-12-22 17:32:38 +0300

    [ANDROID] Creating Complete Order Fragment (#277)
    * added necessary files
    * implementation added to use fab
    * address adapter created
    * address fragment and mock data  created
    * address view model created
    * address card design and floating action button added
    * fab added
    * refactor
    * fab action added
    * fab view bug fixed
    * navigation added
    * add address fragment added
    * nav bugs solved
    * required strings added
    * address list updated
    * address form added
    * card view style added
    * required files added
    * adapter created for card list
    * fragment filled fab inserted
    * view model created
    * credit card item designed
    * bank info fragment designed
    * payment fragment edited
    * navigation created
    * required files added
    * navigations added
    * required strings added
    * styles added
    * button bug fixed
    * binding added + null pointer bug fixed

---
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/AddAddressFragment.kt
@@ -0,0 +1,31 @@
+package com.example.getflix.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.getflix.R
+import com.example.getflix.databinding.FragmentAddAddressBinding
+import com.example.getflix.databinding.FragmentPaymentBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+
+class AddAddressFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        activity?.toolbar!!.toolbar_title.text = getString(R.string.address_add)
+        val binding = DataBindingUtil.inflate<FragmentAddAddressBinding>(inflater, R.layout.fragment_add_address,
+            container, false)
+
+        return binding.root
+    }
+
+
+}
