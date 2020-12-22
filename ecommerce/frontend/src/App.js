e28006a8ee620506e05f9beb774794acb6ac3667
Mehdi Saffar <saffar.mehdi3@gmail.com>
2020-12-22 23:37:21 +0300

    add loading screen
    - loading screen shows fade-in getflix logo
    - loading spinner only appears after 3000ms
    - main layout only loaded when loading=false
    - loading is false when both init and getCategories finishes
    - solves racing conditions

--- ecommerce/frontend/src/App.js
+++ ecommerce/frontend/src/App.js
@@ -4,15 +4,40 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 import { MainLayout } from './components/layout/MainLayout'
 import { Helmet } from 'react-helmet'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useAppContext } from './context/AppContext'
+import { Spin } from 'antd'
+import getflixLogo from './assets/logo.png'
+
+import { LoadingOutlined } from '@ant-design/icons'
+
+const loadingSpinner = <LoadingOutlined style={{ fontSize: 42 }} spin />
+const LoadingScreen = () => {
+    return (
+        <div className="app-loading">
+            <img src={getflixLogo} className="splash-logo"></img>
+        </div>
+    )
+}
 
 function App() {
     const { init, getCategories } = useAppContext()
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        init()
-        getCategories()
+        async function fetch() {
+            console.log('App:useEffect:start')
+            setLoading(true)
+            try {
+                await Promise.all([init(), getCategories()])
+            } catch (error) {
+                console.error('App:useEffect:error', error)
+            } finally {
+                setLoading(false)
+                console.log('App:useEffect:end')
+            }
+        }
+        fetch()
     }, [])
 
     return (
@@ -21,7 +46,13 @@ function App() {
                 <title>Getflix - Get all you want</title>
             </Helmet>
             <Router>
-                <MainLayout />
+                {loading ? (
+                    <Spin className="app-loading-spin" indicator={loadingSpinner} delay={3000} spinning={loading}>
+                        <LoadingScreen />
+                    </Spin>
+                ) : (
+                    <MainLayout key={loading} />
+                )}
             </Router>
         </>
     )
