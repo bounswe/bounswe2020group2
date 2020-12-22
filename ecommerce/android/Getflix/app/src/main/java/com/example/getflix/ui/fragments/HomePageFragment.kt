25064f72020ddc452f74043fb49bdf54740127d2
karalleyna <36455180+karalleyna@users.noreply.github.com>
2020-12-23 00:07:10 +0300

    Adjust home fragment considering HomeCategoriesAdapter

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/HomePageFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/HomePageFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,13 +18,17 @@ import com.example.getflix.ui.adapters.HomeCategoriesAdapter
 import com.example.getflix.ui.adapters.HomeRecommenderAdapter
 import com.example.getflix.ui.adapters.TodaysDealsAdapter
 import com.example.getflix.ui.adapters.TrendingProductAdapter
+import com.example.getflix.ui.fragments.HomePageFragment.StaticData.recyclerViewFirstPosition
 import com.example.getflix.ui.viewmodels.HomeViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 
 class HomePageFragment : Fragment() {
+    object StaticData {
+        var recyclerViewFirstPosition = MutableLiveData<Int>()
 
+    }
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -41,15 +46,32 @@ class HomePageFragment : Fragment() {
         homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         binding.lifecycleOwner = this
 
-        val adapterForHomeCategoriesAdapter = HomeCategoriesAdapter()
+        val adapterForHomeCategoriesAdapter = HomeCategoriesAdapter(viewLifecycleOwner)
         val adapterForTodaysDealsAdapter = TodaysDealsAdapter()
         val adapterForHomeRecommenderAdapter = HomeRecommenderAdapter()
         val adapterForTrendingProductAdapter = TrendingProductAdapter()
 
         binding.categories.adapter = adapterForHomeCategoriesAdapter
-        val layoutManagerForCategoriesAdapter: RecyclerView.LayoutManager =  LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        val layoutManagerForCategoriesAdapter =  LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         binding.categories.layoutManager = layoutManagerForCategoriesAdapter
         adapterForHomeCategoriesAdapter.submitList(categories)
+
+        recyclerViewFirstPosition.value = 0
+        binding.categories.addOnScrollListener( object: RecyclerView.OnScrollListener(){
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                val firstElementPosition = layoutManagerForCategoriesAdapter.findFirstVisibleItemPosition()
+                recyclerViewFirstPosition.value= firstElementPosition
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val firstElementPosition = layoutManagerForCategoriesAdapter.findFirstVisibleItemPosition()
+                recyclerViewFirstPosition.value= firstElementPosition
+
+            }
+        })
+
 
         binding.todaysDeals.adapter = adapterForTodaysDealsAdapter
         val layoutManagerForTodaysDeals: RecyclerView.LayoutManager =  LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
