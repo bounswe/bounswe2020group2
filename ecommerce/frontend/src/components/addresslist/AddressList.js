d1cac70e78fb174452ea8c879aae2a3d3b4b12f6
ozdenizdolu <32651175+ozdenizdolu@users.noreply.github.com>
2020-12-24 13:24:29 +0300

    [FRONTEND] Implement AddressList component in the payment page (#239)
    * Started working on addresslist component on checkout
    - Changed the folder structure of components/checkout (added 2 folders
      cardlist & addresslist in checkout)
    - Minor corrections on creditcardlist
    - Started creating address cards
    * disable payment button when shopping cart empty
    * minor styling changes to address card
    * AddressList component has been completed
    * fix login successful
    * Changed the folder structure(got rid of /checkout/)
    * Now, the payment button don't alert total price
    * Address/card list have been put in a container
    * Create the layout(containers etc) in checkoutPage
    * Changes made according to Abdullah's comments
    - Address edit modal now shows "* marked fields are required"
    - Cancel button says "Cancel" on modals instead of "Go Back"
    * Empty text for addressList is now correct
    * Made the final changes for Mehdi's comments:
    - Deleted the unnecessary <> fragment
    - Got rid of R.omit in the AddressModalInner
    - Deleted the unnecessary empty functions in form
    - Deleted unnecessary comments here and there
    * Finishing touches(Considering Mehdi's comments)
    Co-authored-by: Özdeniz Dolu <ozdendolu@windowslive.com>
    Co-authored-by: Mehdi Saffar <saffar.mehdi3@gmail.com>

---
+++ ecommerce/frontend/src/components/addresslist/AddressList.js
@@ -0,0 +1,117 @@
+import { Button, List, Spin, notification } from 'antd'
+import { sleep } from '../../utils'
+import { useEffect, useState } from 'react'
+import { AddressCard } from './AddressCard'
+import { AddressModal } from './AddressModal'
+import { useAppContext } from '../../context/AppContext'
+import './AddressList.less'
+
+export const AddressList = () => {
+    const [addressList, setAddressList] = useState([])
+    const { user } = useAppContext()
+
+    const getAddressList = async userId => {
+        let addresses = []
+        try {
+            setIsLoading(true)
+            await sleep(2000)
+            addresses = [
+                {
+                    id: 44313,
+                    title: 'Home Address',
+                    phone: {
+                        countryCode: '+90',
+                        number: '5555555555',
+                    },
+                    name: 'Özdeniz',
+                    surname: 'Dolu',
+                    zipCode: '34000',
+                    address: 'React Mah. JS Sk. No: 42/1',
+                    province: 'Besiktas',
+                    city: 'Istanbul',
+                    country: 'Turkey',
+                },
+                {
+                    id: 12322,
+                    title: 'Work Address',
+                    phone: {
+                        countryCode: '+90',
+                        number: '544444444',
+                    },
+                    name: 'Özdeniz',
+                    surname: 'Dolu',
+                    zipCode: '35000',
+                    address: 'Python Mah. Numpy Sk. No: 23/1',
+                    province: 'Besiktas',
+                    city: 'Istanbul',
+                    country: 'Turkey',
+                },
+            ]
+        } catch (error) {
+            notification.warning({ message: 'There was an error with your request.' })
+            console.error(error)
+        } finally {
+            setIsLoading(false)
+            return addresses
+        }
+    }
+
+    useEffect(() => {
+        async function fetch() {
+            const addresses = await getAddressList(user?.id)
+            setAddressList(addresses)
+            setSelectedAddress(addresses.length > 0 ? addresses[0].id : null)
+        }
+        fetch()
+    }, [user])
+
+    const [selectedAddress, setSelectedAddress] = useState()
+    const [addVisible, setAddVisible] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+
+    const onAddressSelect = addressId => setSelectedAddress(addressId)
+
+    const onAddressInfoChange = async () => {
+        const addresses = await getAddressList(user?.id)
+        setAddressList(addresses)
+    }
+
+    return (
+        <div className="addresslist-container">
+            <div className="addresslist-title">
+                Select an address
+                <Button onClick={() => setAddVisible(true)} type="dashed">
+                    Add a new address
+                </Button>
+                <AddressModal
+                    mode="add"
+                    onCancel={() => setAddVisible(false)}
+                    onSuccess={() => {
+                        setAddVisible(false)
+                        onAddressInfoChange()
+                    }}
+                    visible={addVisible}
+                />
+            </div>
+            <div className="addresslist-list">
+                <Spin spinning={isLoading}>
+                    <List
+                        locale={{ emptyText: 'Add a new address!' }}
+                        grid={{ gutter: 0 }}
+                        dataSource={addressList}
+                        renderItem={address => (
+                            <List.Item key={address.id}>
+                                <AddressCard
+                                    address={address}
+                                    onAddressInfoChange={onAddressInfoChange}
+                                    selected={selectedAddress === address.id}
+                                    onSelect={onAddressSelect}
+                                />
+                            </List.Item>
+                        )}
+                    />
+                </Spin>
+            </div>
+        </div>
+    )
+}
