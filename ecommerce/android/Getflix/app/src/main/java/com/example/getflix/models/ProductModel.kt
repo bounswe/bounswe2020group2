ce1ed54c0b9a3826bcf10c3a999614e879580401
karalleyna <36455180+karalleyna@users.noreply.github.com>
2020-12-25 21:01:23 +0300

    Add ProductModel

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/models/ProductModel.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/models/ProductModel.kt
@@ -1,17 +1,13 @@
 package com.example.getflix.models
-
 import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
-
 @Parcelize
 data class ProductModel (
-
-
         @SerializedName("id") val id : Int,
         @SerializedName("name") val name : String,
-        @SerializedName("price") val price : Int,
+        @SerializedName("price") val price : Double,
         @SerializedName("creation_date") val creation_date : String,
         @SerializedName("total_rating") val total_rating : Int,
         @SerializedName("rating_count") val rating_count : Int,
@@ -23,7 +19,8 @@ data class ProductModel (
         @SerializedName("category") val category : CategoryModel,
         @SerializedName("brand") val brand : BrandModel,
         @SerializedName("vendor") val vendor : VendorModel,
-        @SerializedName("rating") val rating : Int,
+        @SerializedName("rating") val rating : Double,
         @SerializedName("images") val images : List<String>,
-        @SerializedName("old_price") val old_price : Int
+        @SerializedName("price_after_discount") val priceDiscounted : Double,
+        @SerializedName("is_deleted") val deleted : Boolean
 ) : Parcelable
