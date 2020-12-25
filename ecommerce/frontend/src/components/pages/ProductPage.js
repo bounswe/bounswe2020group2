011f8c621d58f013aca6b4a05a8a3bd92f116a0e
vaveylafv <44326869+vaveylafv@users.noreply.github.com>
2020-12-25 20:47:17 +0300

    husky formatting

--- ecommerce/frontend/src/components/pages/ProductPage.js
+++ ecommerce/frontend/src/components/pages/ProductPage.js
@@ -22,31 +22,31 @@ export const ProductPage = props => {
         query: values.search?.query,
         // query: this can be undefined it means doesn't matter
         // query: for now, case-insensitive search in title or description
-    
+
         category: values.filters?.category,
         // category: if missing, assume all products
-    
+
         subcategory: values.filters?.subcategory,
         // subcategory: if missing, assume all subcategories of the category
         // subcategory: subcategory cannot be given without specifying category
-    
+
         brand: values.filters?.brands,
         // brand: OR semantic
         // brand: these are brand ids I get from the database
         // brand: if brand is undefined or empty array then consider it as any brand
-    
+
         max_price: values.filters?.maxPrice,
         // max_price: if missing, assume +infinity
-    
+
         min_rating: values.filters?.rating,
         // min_rating: if missing, assume 0
         // min_rating: min 0, max 5
         // min_rating: >= semantic
-    
+
         // == sorting ==
         sort_by: values.filters?.sortBy,
         // sort_by: if missing, assume 'best-sellers'
-    
+
         sort_order: 'increasing',
         // sort_order: if missing, assume 'increasing'
         // sort_order: decreasing best-sellers -> best sellers shown first
@@ -54,12 +54,12 @@ export const ProductPage = props => {
         // sort_order: increasing price -> low price products first
         // sort_order: increasing average-customer-review -> low review first
         // sort_order: increasing number-of-comments -> low comments first
-    
+
         // == pagination ==
         page: values.pagination?.current,
         // page: pages start from 0
         // page: if missing, assume 0
-    
+
         page_size: values.pagination?.pageSize,
         // page_size: smallest page_size should 1, biggest should be 100
         // page_size: if missing, assume 10
@@ -78,7 +78,7 @@ export const ProductPage = props => {
                     category: data.category.id,
                     sortBy: 'best-sellers',
                     subcategory: data.subcategory.id,
-                    type: 'products'
+                    type: 'products',
                 }
                 const values = {
                     search: { query: initialValues.query, type: initialValues.type },
@@ -116,9 +116,7 @@ export const ProductPage = props => {
             </div>
             <h2 className="best-sellers-topic">Best Sellers in {subcategory}</h2>
             <div className="best-sellers">
-                {bestSellers.map((
-                    product,
-                ) => (
+                {bestSellers.map(product => (
                     <a key={product.id}>
                         <ProductCard product={product} />
                     </a>
