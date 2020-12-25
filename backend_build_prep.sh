373b710e8316c5bb100c682325298eec7c7243ae
Mehdi Saffar <saffar.mehdi3@gmail.com>
2020-12-25 12:58:38 +0300

    fix typo in backend_build_prep.sh

--- backend_build_prep.sh
+++ backend_build_prep.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"     
 
-cp $DIR/secrets/backenv.env.production $DIR/ecommerce/backend/.env.production -v
+cp $DIR/secrets/backend.env.production $DIR/ecommerce/backend/.env.production -v
