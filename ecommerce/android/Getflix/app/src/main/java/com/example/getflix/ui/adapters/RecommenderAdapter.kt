85561834cbb601b9a8606db8c5e420d7212cf5bc
karalleyna <36455180+karalleyna@users.noreply.github.com>
2020-12-26 14:18:30 +0300

    Add binding and validation of the response

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/adapters/RecommenderAdapter.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/adapters/RecommenderAdapter.kt
@@ -6,10 +6,11 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.getflix.activities.MainActivity
 import com.example.getflix.addToShoppingCart
 import com.example.getflix.databinding.CardRecommendedProductBinding
-import com.example.getflix.databinding.FragmentProductBinding
 import com.example.getflix.models.ProductModel
+import com.squareup.picasso.Picasso
 
 class RecommenderAdapter :
     ListAdapter<ProductModel, RecommenderAdapter.ViewHolder>(RecommenderDiffCallback()) {
@@ -21,23 +22,42 @@ class RecommenderAdapter :
     }
 
     private fun ViewHolder.bind(product: ProductModel) {
+        Picasso.get().load(product.images[0]).into(binding.productImage)
+
         var amount = binding.amountRecProduct.text.toString().toInt()
+
         binding.product = product
-        binding.oldProductPrice.text = product.price.toString() + " TL"
+        if (product.price.toString().length > 5) {
+            binding.oldProductPrice.text = product.price.toString().substring(0, 5) + " TL"
+        } else {
+            binding.oldProductPrice.text = product.price.toString() + " TL"
+
+        }
         binding.oldProductPrice.setPaintFlags(binding.oldProductPrice.getPaintFlags() or Paint.STRIKE_THRU_TEXT_FLAG)
-        binding.productPrice.text = product.priceDiscounted.toString() + " TL"
-        binding.addShopping.setOnClickListener {
-            addToShoppingCart(amount, 1, product.id)
+        if (product.priceDiscounted.toString().length > 5) {
+            binding.productPrice.text =
+                product.priceDiscounted.toString().substring(0, 5) + " TL"
+
+        } else {
+            binding.productPrice.text = product.priceDiscounted.toString() + " TL"
+
+        }
+        if (MainActivity.StaticData.isCustomer) {
+            binding.addShopping.setOnClickListener {
+                addToShoppingCart(amount, 1, product.id)
+            }
         }
         binding.decreaseRecProduct.setOnClickListener {
             if (amount > 1) {
                 amount = amount.dec()
+                binding.amountRecProduct.text = amount.toString()
             }
         }
         binding.increaseRecProduct.setOnClickListener {
             amount = amount.inc()
+            binding.amountRecProduct.text = amount.toString()
+
         }
-        binding.amountRecProduct.text = amount.toString()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
