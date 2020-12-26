9a7856a717ff1b8b1132cc03358eafebf06768c9
Özdeniz Dolu <ozdendolu@windowslive.com>
2020-12-26 05:07:13 +0300

    Add Terms and Conditions component

--- ecommerce/frontend/src/components/pages/CheckoutPage.js
+++ ecommerce/frontend/src/components/pages/CheckoutPage.js
@@ -1,12 +1,21 @@
 import { CreditCardList } from '../cardlist/CreditCardList'
 import { AddressList } from '../addresslist/AddressList'
 import "./CheckoutPage.less"
+import { CheckoutTermsAndConditions } from '../distant_sales_agreement/CheckoutTermsAndConditions'
+import { useEffect, useState } from 'react'
 export const CheckoutPage = () => {
+
+    const [selectedCard, setSelectedCard] = useState()
+    const [selectedAddress, setSelectedAddress] = useState()
+    const [userAgreed, setUserAgreed] = useState(false)
+
+
     return (
         <div className="checkout-page-container">
             <div className="payment-options-container">
-                <CreditCardList />
-                <AddressList />
+                <CreditCardList onSelect={setSelectedCard} />
+                <AddressList onSelect={setSelectedAddress} />
+                <CheckoutTermsAndConditions onChange={setUserAgreed} />
             </div>
             <div className="price-summary-container">
                 Price summary
