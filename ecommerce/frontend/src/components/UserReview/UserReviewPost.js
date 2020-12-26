c140415564b115c69627937b324a5a58088b989b
Mehdi Saffar <saffar.mehdi3@gmail.com>
2020-12-26 17:46:26 +0300

    fix user id, loading to button, fix null comment

--- ecommerce/frontend/src/components/UserReview/UserReviewPost.js
+++ ecommerce/frontend/src/components/UserReview/UserReviewPost.js
@@ -6,87 +6,36 @@ import { api } from '../../api'
 import './UserReviewPost.less'
 import { useAppContext } from '../../context/AppContext'
 const { TextArea } = Input
-export const UserReviewPost = ({
-    width = '500px',
-    product = {
-        id: 1,
-        name: "Under Armour Men's Tech 2.0 Short Sleeve T-Shirt",
-        price: 18,
-        creation_date: '2019-08-20T07:22:34Z',
-        total_rating: 80,
-        rating_count: 20,
-        stock_amount: 10,
-        short_description: '100% Polyester, Machine Washable, Material wicks sweat & dries really fast',
-        subcategory: {
-            name: "Men's Fashion",
-            id: 10,
-        },
-        long_description: 'long description',
-        discount: 0.1,
-        category: {
-            id: 4,
-            name: 'Clothing',
-        },
-        brand: {
-            name: 'Under Armour',
-            id: 1,
-        },
-        vendor: {
-            rating: 4.666666666666667,
-            id: 1,
-            name: 'omerfaruk deniz',
-        },
-        rating: 4,
-        images: [
-            'https://underarmour.scene7.com/is/image/Underarmour/V5-1345317-014_FC?rp=standard-0pad|pdpMainDesktop&scl=1&fmt=jpg&qlt=85&resMode=sharp2&cache=on,on&bgc=F0F0F0&wid=566&hei=708&size=566,708',
-            'https://underarmour.scene7.com/is/image/Underarmour/V5-1326413-001_FC_Main?rp=standard-0pad|pdpMainDesktop&scl=1&fmt=jpg&qlt=85&resMode=sharp2&cache=on,on&bgc=F0F0F0&wid=566&hei=708&size=566,708',
-            'https://images-na.ssl-images-amazon.com/images/I/51tb0KkLYqL._AC_UX679_.jpg',
-        ],
-        old_price: 20,
-    },
-}) => {
+
+export const UserReviewPost = ({ product }) => {
     const [isLoading, setIsLoading] = useState(false)
     const [comment, setComment] = useState('')
     const [rating, setRating] = useState(null)
-    const { user: user_id } = useAppContext()
-    const onChangeComment = e => {
-        setComment(e.target.value)
-    }
-    const onChangeRating = e => {
-        setRating(e)
-    }
-    const onPressReview = async () => {
-        if (!rating || comment == '') {
-            if (comment == '') {
-                notification.warning({ description: 'Please write a comment' })
-            }
-            if (!rating) {
-                notification.warning({ description: 'Please rate the product' })
-            }
-        } else {
-            try {
-                setIsLoading(true)
-                const { data } = await api.post(`/review`, {
-                    user_id: user_id,
-                    product_id: product.id,
-                    vendor_id: product.vendor.id,
-                    rating: rating,
-                    comment: comment,
-                })
-                console.log('data', data)
-            } catch (error) {
-                if (error.response.status === 403) {
-                    notification.warning({ description: 'You have to buy the product to make a comment' })
-                }
-            } finally {
-                setIsLoading(false)
-            }
+    const { user } = useAppContext()
 
-            // notification.success({ description: 'Successfully reviewed' })
+    const onChangeComment = e => setComment(e.target.value)
+    const onChangeRating = e => setRating(e)
+
+    const onPressReview = async () => {
+        if (!rating) return notification.warning({ description: 'Please rate the product' })
+
+        try {
+            setIsLoading(true)
+            await api.post(`/review`, {
+                user_id: user.id,
+                product_id: product.id,
+                rating: rating,
+                comment: comment,
+            })
+        } catch (error) {
+            if (error.response.status === 403)
+                notification.warning({ description: 'You have to buy the product to make a comment' })
+        } finally {
+            setIsLoading(false)
         }
     }
     return (
-        <div className="post-review-product" style={{ maxWidth: width }}>
+        <div className="post-review-product">
             <div className="post-review-item">
                 <div className="cart-item-picture">
                     <Link to={`/product/${product.id}`}>
@@ -98,7 +47,7 @@ export const UserReviewPost = ({
                 </div>
                 <div className="post-review-description">
                     <div className="post-review-title">
-                        <Link to={`/product/${product.id}`}>{product.name ?? 'title'}</Link>
+                        <Link to={`/product/${product.id}`}>{product.title ?? 'title'}</Link>
                     </div>
                     <div className="post-review-short-description">{product.short_description}</div>
                     <div className="post-review-rate">
@@ -120,11 +69,9 @@ export const UserReviewPost = ({
                 />
             </div>
             <div className="post-review-button">
-                <Spin spinning={isLoading}>
-                    <Button type="primary" onClick={onPressReview}>
-                        Send Review
-                    </Button>
-                </Spin>
+                <Button loading={isLoading} type="primary" onClick={onPressReview}>
+                    Send Review
+                </Button>
             </div>
         </div>
     )
--- ecommerce/frontend/src/components/layout/Content.js
+++ ecommerce/frontend/src/components/layout/Content.js
@@ -3,6 +3,9 @@ import './Layout_common.less'
 import { Layout } from 'antd'
 import { Redirect, Route, Switch } from 'react-router-dom'
 
+import { useAppContext } from '../../context/AppContext'
+import { CategoryBar } from '../CategoryBar'
+import { CheckoutPage } from '../pages/CheckoutPage'
 import { HomePage } from '../pages/HomePage'
 import { LoginPage } from '../pages/LoginPage'
 import { ProductPage } from '../pages/ProductPage'
@@ -10,10 +13,6 @@ import { ProfilePage } from '../pages/ProfilePage'
 import { SearchPage } from '../pages/SearchPage'
 import { SignupPage } from '../pages/SignupPage'
 import { ShoppingCartPage } from '../ShoppingCart/ShoppingCartPage'
-import { CategoryBar } from '../CategoryBar'
-import { CheckoutPage } from '../pages/CheckoutPage'
-import { UserReviews } from '../UserReview/UserReviews'
-import { useAppContext } from '../../context/AppContext'
 
 export const Content = () => {
     const { user } = useAppContext()
