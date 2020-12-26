c83bc622b000884f6cf7af20573d7e2b953ff32a
hande <hansirikci@gmail.com>
2020-12-26 22:56:24 +0300

    orders fragment edited

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/OrderInfoFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/OrderInfoFragment.kt
@@ -7,16 +7,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.getflix.R
 import com.example.getflix.databinding.FragmentBankAccountBinding
 import com.example.getflix.databinding.FragmentOrderInfoBinding
+import com.example.getflix.models.CardModel
+import com.example.getflix.models.OrderModel
+import com.example.getflix.ui.adapters.OrdersAdapter
 import com.example.getflix.ui.fragments.OrderInfoFragmentDirections.Companion.actionOrderInfoFragmentToProfileFragment
+import com.example.getflix.ui.viewmodels.CreditCardViewModel
+import com.example.getflix.ui.viewmodels.OrderViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 class OrderInfoFragment : Fragment() {
 
+    private lateinit var viewModel: OrderViewModel
+    private lateinit var binding: FragmentOrderInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,9 +48,11 @@ class OrderInfoFragment : Fragment() {
                 }
             }
         )
+        val orders = arrayListOf<OrderModel>()
+        //val orderAdapter = OrdersAdapter(orders)
+        //binding.ordersList.adapter = orderAdapter
+        binding.ordersList.setHasFixedSize(true)
 
         return binding.root
     }
-
-
 }
