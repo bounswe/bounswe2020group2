07657f64a150bf3993a6b26c9f7c10cb80eebe2f
Abdullah Yildiz <abdullahyildiz321@gmail.com>
2020-12-26 14:02:45 +0300

    Backend integration user post review

--- ecommerce/frontend/src/components/UserReview/UserReviewPost.js
+++ ecommerce/frontend/src/components/UserReview/UserReviewPost.js
@@ -1,26 +1,88 @@
 import React, { useEffect, useState } from 'react'
-import { Pagination, Spin, Button, Input, Rate, notification } from 'antd'
+import { Spin, Button, Input, Rate, notification } from 'antd'
 import { Link } from 'react-router-dom'
 import './UserReviews.less'
 import { api } from '../../api'
 import './UserReviewPost.less'
-import { product } from '../../mocks/mocks'
-import { round } from '../../utils'
+import { useAppContext } from '../../context/AppContext'
 const { TextArea } = Input
-export const UserReviewPost = (width = '500px') => {
-    const [isLoading, setIsLoading] = useState(true)
+export const UserReviewPost = ({
+    width = '500px',
+    product = {
+        id: 1,
+        name: "Under Armour Men's Tech 2.0 Short Sleeve T-Shirt",
+        price: 18,
+        creation_date: '2019-08-20T07:22:34Z',
+        total_rating: 80,
+        rating_count: 20,
+        stock_amount: 10,
+        short_description: '100% Polyester, Machine Washable, Material wicks sweat & dries really fast',
+        subcategory: {
+            name: "Men's Fashion",
+            id: 10,
+        },
+        long_description: 'long description',
+        discount: 0.1,
+        category: {
+            id: 4,
+            name: 'Clothing',
+        },
+        brand: {
+            name: 'Under Armour',
+            id: 1,
+        },
+        vendor: {
+            rating: 4.666666666666667,
+            id: 1,
+            name: 'omerfaruk deniz',
+        },
+        rating: 4,
+        images: [
+            'https://underarmour.scene7.com/is/image/Underarmour/V5-1345317-014_FC?rp=standard-0pad|pdpMainDesktop&scl=1&fmt=jpg&qlt=85&resMode=sharp2&cache=on,on&bgc=F0F0F0&wid=566&hei=708&size=566,708',
+            'https://underarmour.scene7.com/is/image/Underarmour/V5-1326413-001_FC_Main?rp=standard-0pad|pdpMainDesktop&scl=1&fmt=jpg&qlt=85&resMode=sharp2&cache=on,on&bgc=F0F0F0&wid=566&hei=708&size=566,708',
+            'https://images-na.ssl-images-amazon.com/images/I/51tb0KkLYqL._AC_UX679_.jpg',
+        ],
+        old_price: 20,
+    },
+}) => {
+    const [isLoading, setIsLoading] = useState(false)
     const [comment, setComment] = useState('')
     const [rating, setRating] = useState(null)
-    const onChange = e => {
-        // setComment(value)
-        console.log(e)
+    const { user: user_id } = useAppContext()
+    const onChangeComment = e => {
+        setComment(e.target.value)
     }
-    const onPressReview = () => {
-        if (!rating) {
-            notification.warning({ description: 'Please rate the product' })
-        }
-        if (comment == '') {
-            notification.warning({ description: 'Please comment the product' })
+    const onChangeRating = e => {
+        setRating(e)
+    }
+    const onPressReview = async () => {
+        if (!rating || comment == '') {
+            if (comment == '') {
+                notification.warning({ description: 'Please write a comment' })
+            }
+            if (!rating) {
+                notification.warning({ description: 'Please rate the product' })
+            }
+        } else {
+            try {
+                setIsLoading(true)
+                const { data } = await api.post(`/review`, {
+                    user_id: user_id,
+                    product_id: product.id,
+                    vendor_id: product.vendor.id,
+                    rating: rating,
+                    comment: comment,
+                })
+                console.log('data', data)
+            } catch (error) {
+                if (error.response.status === 403) {
+                    notification.warning({ description: 'You have to buy the product to make a comment' })
+                }
+            } finally {
+                setIsLoading(false)
+            }
+
+            // notification.success({ description: 'Successfully reviewed' })
         }
     }
     return (
@@ -35,13 +97,12 @@ export const UserReviewPost = (width = '500px') => {
                     </Link>
                 </div>
                 <div className="post-review-description">
-                    <div className="cart-item-title">
+                    <div className="post-review-title">
                         <Link to={`/product/${product.id}`}>{product.name ?? 'title'}</Link>
                     </div>
-                    <div className="cart-item-description">{product.short_description}</div>
+                    <div className="post-review-short-description">{product.short_description}</div>
                     <div className="post-review-rate">
-                        {'Please rate this product'}
-                        <Rate />
+                        <Rate onChange={onChangeRating} />
                     </div>
                 </div>
             </div>
@@ -52,13 +113,18 @@ export const UserReviewPost = (width = '500px') => {
                     showCount
                     rows={4}
                     maxLength={500}
+                    value={comment}
                     placeholder="Did you like the product?"
-                    onChange={onChange}
+                    onChange={onChangeComment}
                 />
             </div>
-            <Button type="primary" onClick={onPressReview}>
-                Send Review
-            </Button>
+            <div className="post-review-button">
+                <Spin spinning={isLoading}>
+                    <Button type="primary" onClick={onPressReview}>
+                        Send Review
+                    </Button>
+                </Spin>
+            </div>
         </div>
     )
 }
