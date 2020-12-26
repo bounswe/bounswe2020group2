708b655a4d27ce68fd98f39f12c75fcb0fa4d180
karalleyna <36455180+karalleyna@users.noreply.github.com>
2020-12-26 11:32:39 +0300

    Implement RecommenderAdapter from scratch

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/adapters/RecommenderAdapter.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/adapters/RecommenderAdapter.kt
@@ -1,16 +1,18 @@
 package com.example.getflix.ui.adapters
 
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.getflix.addToShoppingCart
 import com.example.getflix.databinding.CardRecommendedProductBinding
 import com.example.getflix.databinding.FragmentProductBinding
 import com.example.getflix.models.ProductModel
 
 class RecommenderAdapter :
-        ListAdapter<ProductModel, RecommenderAdapter.ViewHolder>(RecommenderDiffCallback()) {
+    ListAdapter<ProductModel, RecommenderAdapter.ViewHolder>(RecommenderDiffCallback()) {
 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -19,7 +21,23 @@ class RecommenderAdapter :
     }
 
     private fun ViewHolder.bind(product: ProductModel) {
+        var amount = binding.amountRecProduct.text.toString().toInt()
         binding.product = product
+        binding.oldProductPrice.text = product.price.toString() + " TL"
+        binding.oldProductPrice.setPaintFlags(binding.oldProductPrice.getPaintFlags() or Paint.STRIKE_THRU_TEXT_FLAG)
+        binding.productPrice.text = product.priceDiscounted.toString() + " TL"
+        binding.addShopping.setOnClickListener {
+            addToShoppingCart(amount, 1, product.id)
+        }
+        binding.decreaseRecProduct.setOnClickListener {
+            if (amount > 1) {
+                amount = amount.dec()
+            }
+        }
+        binding.increaseRecProduct.setOnClickListener {
+            amount = amount.inc()
+        }
+        binding.amountRecProduct.text = amount.toString()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,7 +45,7 @@ class RecommenderAdapter :
     }
 
     class ViewHolder private constructor(val binding: CardRecommendedProductBinding) :
-            RecyclerView.ViewHolder(binding.root) {
+        RecyclerView.ViewHolder(binding.root) {
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
