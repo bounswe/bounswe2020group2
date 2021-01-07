c8191354a5b036ed41f7df049a2650a04456537a
Abdullah Yildiz <abdullahyildiz321@gmail.com>
2021-01-07 20:22:46 +0300

    verified notif. when guest bug

--- ecommerce/frontend/src/components/layout/Content.js
+++ ecommerce/frontend/src/components/layout/Content.js
@@ -29,7 +29,7 @@ export const Content = () => {
     return (
         <Layout.Content className="content">
             <CategoryBar />
-            {location.pathname.startsWith('/profile') || <IsVerifiedNotification />}
+            {isUser && user.is_verified && <IsVerifiedNotification />}
 
             <Switch>
                 {isUser && <Route path="/verify/:id" component={EmailVerification} />}
