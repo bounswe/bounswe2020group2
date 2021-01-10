5632490e19ae31f014db62dcfe4e19617c694501
hande <hansirikci@gmail.com>
2021-01-10 18:51:41 +0300

    list product model created

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/models/ListProductModel.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/models/ListProductModel.kt
@@ -1,4 +1,14 @@
 package com.example.getflix.models
 
-class ListProductModel {
-}
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class ListProductModel (
+
+    @SerializedName("id")
+    val id : Int,
+    @SerializedName("product")
+    val product : ProductModel
+) : Parcelable
