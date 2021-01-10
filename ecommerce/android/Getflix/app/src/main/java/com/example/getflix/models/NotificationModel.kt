14e28cd0bec470196d235942433a369acfe6c351
hande <hansirikci@gmail.com>
2021-01-10 20:19:52 +0300

    notification model created

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/models/NotificationModel.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/models/NotificationModel.kt
@@ -1,4 +1,11 @@
 package com.example.getflix.models
 
-class NotificationModel {
-}
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class NotificationModel(
+    @SerializedName("id") val id: Int,
+    @SerializedName("name") val name: String,
+) : Parcelable
