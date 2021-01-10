cefb23a8e515311e341c030f1e059ea33b9e2998
hande <hansirikci@gmail.com>
2021-01-10 20:20:24 +0300

    notification fragment created

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/NotificationFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/NotificationFragment.kt
@@ -5,56 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.getflix.R
+import com.example.getflix.databinding.FragmentListsBinding
+import com.example.getflix.databinding.FragmentNotificationBinding
+import com.example.getflix.models.*
+import com.example.getflix.ui.adapters.ListsAdapter
+import com.example.getflix.ui.adapters.NotificationAdapter
+import com.example.getflix.ui.viewmodels.ListViewModel
+import com.example.getflix.ui.viewmodels.NotificationViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NotificationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NotificationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var viewModel: NotificationViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false)
+        val binding = DataBindingUtil.inflate<FragmentNotificationBinding>(
+            inflater, R.layout.fragment_notification,
+            container, false
+        )
+
+        viewModel = ViewModelProvider(this).get(NotificationViewModel::class.java)
+        binding.viewmodel = ListViewModel()
+        activity?.toolbar!!.toolbar_title.text = getString(R.string.notifications)
+        val recView = binding?.notificationList as RecyclerView
+
+        var not1 =
+            NotificationModel(10, "Discount!")
+        var not2 =
+            NotificationModel(10, "Discount!")
+        val nots = arrayListOf<NotificationModel>(not1, not2)
+        val notAdapter = NotificationAdapter(nots)
+        recView.adapter = notAdapter
+        recView.setHasFixedSize(true)
+
+        for (not in nots) {
+            viewModel.addList(not)
+        }
+
+        viewModel.notificationList.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                notAdapter.submitList(it)
+            }
+        })
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NotificationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NotificationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
