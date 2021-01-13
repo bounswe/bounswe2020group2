07908fbad790a2a181ebf787e11e05b650b9dfb9
abdullahyldz <abdullahyildiz321@gmail.com>
2021-01-14 01:39:20 +0300

    rating bug fit content

--- ecommerce/frontend/src/components/VendorContent/VendorPageContent.js
+++ ecommerce/frontend/src/components/VendorContent/VendorPageContent.js
@@ -6,14 +6,14 @@ import { useState } from 'react'
 import { ProductCard } from '../product_card/ProductCard'
 import { useAppContext } from '../../context/AppContext'
 
-export const VendorPageContent = ({ products, editable = true }) => {
+export const VendorPageContent = ({ products }) => {
     const { user } = useAppContext()
 
     return (
         <div className="vendor-page-results">
             <div className="vendor-page-results-product-grid">
                 {products.map(product => (
-                    <ProductCard key={product.id} product={product} editable={editable} />
+                    <ProductCard key={product.id} product={product} />
                 ))}
             </div>
         </div>
