9c51f13d980f6279628040d3a7921d4ce0ca4594
karalleyna <36455180+karalleyna@users.noreply.github.com>
2021-01-15 20:26:12 +0300

    Add firebase connection and fixes bug about the user who already exists

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/RegisterFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/RegisterFragment.kt
@@ -23,113 +23,154 @@ class RegisterFragment : Fragment() {
     private lateinit var binding: FragmentRegisterBinding
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_register,
-                container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_register,
+            container, false
+        )
         registerViewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
         binding.lifecycleOwner = this
         var customer = true
 
         binding.radioGroup.setOnCheckedChangeListener(
-                RadioGroup.OnCheckedChangeListener { group, checkedId ->
-                    when (checkedId) {
-                        R.id.vendor -> {
-                            binding.addressZip.visibility = View.VISIBLE
-                            binding.cityState.visibility = View.VISIBLE
-                            customer = false
-                        }
-                        R.id.customer -> {
-                            binding.addressZip.visibility = View.GONE
-                            binding.cityState.visibility = View.GONE
-                            customer = true
-
-                        }
+            RadioGroup.OnCheckedChangeListener { group, checkedId ->
+                when (checkedId) {
+                    R.id.vendor -> {
+                        binding.addressZip.visibility = View.VISIBLE
+                        binding.cityState.visibility = View.VISIBLE
+                        customer = false
                     }
-                })
+                    R.id.customer -> {
+                        binding.addressZip.visibility = View.GONE
+                        binding.cityState.visibility = View.GONE
+                        customer = true
+
+                    }
+                }
+            })
 
 
-        var checkFields = false
+
         binding.btnRegister.setOnClickListener {
-            activity?.loading_progress!!.visibility = View.VISIBLE
-            checkFields = registerViewModel.setSignUpCredentials(this,
-                            binding.username.text.toString(),
-                            binding.mail.text.toString(),
-                            binding.password.text.toString(),
-                            binding.name.text.toString(),
-                            binding.surname.text.toString(),
-                            binding.phone.text.toString(),
-                            binding.conPassword.text.toString()
-                    )
+            var checkFields = true
 
+            activity?.loading_progress!!.visibility = View.VISIBLE
+
+            if (binding.username.text.toString().isEmpty()) {
+                checkFields = false
+                binding.username.error = getString(R.string.reg_error)
+            }
+            if (binding.mail.text.toString().isEmpty()) {
+                checkFields = false
+                binding.mail.error = getString(R.string.reg_error)
+            }
+            if (binding.password.text.toString().isEmpty()) {
+                checkFields = false
+                binding.password.error = getString(R.string.reg_error)
+            }
+            if (binding.name.text.toString().isEmpty()) {
+                checkFields = false
+                binding.name.error = getString(R.string.reg_error)
+            }
+            if (binding.surname.text.toString().isEmpty()) {
+                checkFields = false
+                binding.surname.error = getString(R.string.reg_error)
+            }
+            if (binding.phone.text.toString().isEmpty()) {
+                checkFields = false
+                binding.phone.error = getString(R.string.reg_error)
+            }
             if (binding.conPassword.text.toString().isEmpty()) {
+                checkFields = false
                 binding.conPassword.error = getString(R.string.reg_error)
             }
 
             var prevAlert = false
-            if (binding.password.text.toString()!=binding.conPassword.text.toString()) {
+            if (binding.password.text.toString() != binding.conPassword.text.toString()) {
                 prevAlert = true
-                infoAlert(this,getString(R.string.confirm_pass_warning))
+                infoAlert(this, getString(R.string.confirm_pass_warning))
             }
 
-            if (binding.password.text.toString().length<8) {
-                if(!prevAlert) {
+            if (binding.password.text.toString().length < 8) {
+                if (!prevAlert) {
                     infoAlert(this, getString(R.string.pass_char_limit))
                     prevAlert = true
                 }
             }
 
-            if (binding.username.text.toString().length<6) {
-                if(!prevAlert) {
+            if (binding.username.text.toString().length < 6) {
+                if (!prevAlert) {
                     infoAlert(this, getString(R.string.username_char_limit))
                     prevAlert = true
                 }
             }
 
-            if (binding.name.text.toString().length<=2 || binding.surname.text.toString().length<=2) {
-                if(!prevAlert)
-                    infoAlert(this,getString(R.string.valid_name))
+            if (binding.name.text.toString().length <= 2 || binding.surname.text.toString().length <= 2) {
+                if (!prevAlert) {
+                    infoAlert(this, getString(R.string.valid_name))
+                    prevAlert = true
+                }
             }
 
             if (!binding.check.isChecked) {
                 checkFields = false
                 binding.check.error = getString(R.string.reg_agree_err)
-                activity?.loading_progress!!.visibility = View.GONE
             }
             if (!customer) {
                 if (!binding.maddress.text.toString().isEmpty()) {
+                    checkFields = false
                     binding.maddress.error = getString(R.string.reg_error)
-                    activity?.loading_progress!!.visibility = View.GONE
                 }
                 if (!binding.zipCode.text.toString().isEmpty()) {
+                    checkFields = false
                     binding.maddress.error = getString(R.string.reg_error)
-                    activity?.loading_progress!!.visibility = View.GONE
                 }
                 if (!binding.city.text.toString().isEmpty()) {
+                    checkFields = false
                     binding.city.error = getString(R.string.reg_error)
-                    activity?.loading_progress!!.visibility = View.GONE
                 }
                 if (!binding.state.text.toString().isEmpty()) {
+                    checkFields = false
                     binding.state.error = getString(R.string.reg_error)
-                    activity?.loading_progress!!.visibility = View.GONE
                 }
+            }
+            if (checkFields && prevAlert.not()) {
+                registerViewModel.setSignUpCredentials(
+                    this,
+                    binding.username.text.toString(),
+                    binding.mail.text.toString(),
+                    binding.password.text.toString(),
+                    binding.name.text.toString(),
+                    binding.surname.text.toString(),
+                    binding.phone.text.toString(),
+                    binding.conPassword.text.toString()
+                )
+
+            } else {
+                requireActivity().loading_progress!!.visibility = View.GONE
             }
 
         }
         registerViewModel.canSignUp.observe(viewLifecycleOwner, Observer {
-            if (it != null && it.successful && checkFields) {
-                activity?.loading_progress!!.visibility = View.GONE
-                doneAlert(this,getString(R.string.register_success),::navigateLogin)
+            activity?.loading_progress!!.visibility = View.GONE
+
+            if (it != null && it.message == "Username is already in use") {
+                infoAlert(this, getString(R.string.user_already_exists))
+                navigateLogin()
+            } else if (it != null && it.successful) {
+                println("oturum acıyor")
+                // doneAlert()
+
+            } else {
+                println("nasil yani")
             }
         })
 
         binding.btnBack.setOnClickListener {
             view?.findNavController()?.popBackStack()
         }
-
-
-
 
         return binding.root
     }
