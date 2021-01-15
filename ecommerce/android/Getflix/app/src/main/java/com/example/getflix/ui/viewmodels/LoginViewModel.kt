584ccfa375143dc19ebb385006faa2801fc56e99
karalleyna <36455180+karalleyna@users.noreply.github.com>
2021-01-15 20:26:52 +0300

    Check both firebase user and backend

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/viewmodels/LoginViewModel.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/viewmodels/LoginViewModel.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.getflix.R
+import com.example.getflix.activities.MainActivity
+import com.example.getflix.activities.MainActivity.StaticData.auth
 import com.example.getflix.infoAlert
 import com.example.getflix.models.*
 import com.example.getflix.service.GetflixApi
@@ -23,8 +25,6 @@ import retrofit2.Response
 class LoginViewModel : ViewModel() {
 
     private val _logReq = MutableLiveData<LoginRequest?>()
-    val loginRequest: LiveData<LoginRequest?>
-        get() = _logReq
 
     private val _onLogin = MutableLiveData<Boolean>()
     val onLogin: LiveData<Boolean>
@@ -36,28 +36,83 @@ class LoginViewModel : ViewModel() {
 
 
     private fun getUserFromBackend(fragment: Fragment) {
-        GetflixApi.getflixApiService.getUserInformation(_logReq.value!!)
-                .enqueue(object :
-                        Callback<LoginResponse> {
-                    override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                        _logReq.value = null
-                        _onLogin.value = false
-                    }
+        /*
+          val user = MainActivity.StaticData.auth.currentUser
+          var emailVerified = user?.isEmailVerified
 
-                    override fun onResponse(
-                        call: Call<LoginResponse>,
-                        response: Response<LoginResponse>
-                    ) {
-                        if (response.body()!!.status.message == "Giriş başarılı") {
-                            _onLogin.value = true
-                            _user.value = response.body()!!.user
-                        } else {
-                            infoAlert(fragment, fragment.requireContext().getString(R.string.login_info))
-                            fragment.requireActivity().loading_progress!!.visibility = View.GONE
+          if (emailVerified != null) {
+              if (emailVerified.not()){
+                  _logReq.value = null
+                  infoAlert(fragment, fragment.requireContext().getString(R.string.login_info))
+                  fragment.requireActivity().loading_progress!!.visibility = View.GONE
+                  return
+              }
+          }
+  */
+        GetflixApi.getflixApiService.getUserInformation(_logReq.value!!)
+            .enqueue(object :
+                Callback<LoginResponse> {
+                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                    val currentUser = call.execute().body()?.user
+                    if (currentUser != null) {
+                        auth.signInWithEmailAndPassword(
+                            currentUser.email,
+                            _logReq.value!!.password
+                        ).addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                _onLogin.value = true
+
+                            } else {
+                                _user.value = null
+                            }
                         }
+                    } else {
+
+                        _user.value = null
+                    }
+                    _logReq.value = null
+                    _onLogin.value = false
+
+                }
+
+                override fun onResponse(
+                    call: Call<LoginResponse>,
+                    response: Response<LoginResponse>
+
+                ) {
+
+                    if (response.body() != null && response.body()!!.status.message == "Giriş başarılı") {
+                        val currentUser = response.body()?.user
+                        if (currentUser != null) {
+                            auth.signInWithEmailAndPassword(
+                                currentUser.email,
+                                _logReq.value!!.password
+                            ).addOnCompleteListener {
+                                if (it.isSuccessful) {
+                                    _user.value = currentUser
+                                    _onLogin.value = true
+
+                                } else {
+                                    _user.value = null
+                                    _onLogin.value = false
+                                }
+                            }
+                        } else {
+                            _onLogin.value = false
+                            _user.value = null
+                        }
+                    } else {
+                        _onLogin.value = false
+                        _user.value = null
+                        infoAlert(
+                            fragment,
+                            fragment.requireContext().getString(R.string.login_info)
+                        )
+                        fragment.requireActivity().loading_progress!!.visibility = View.GONE
                     }
                 }
-                )
+            }
+            )
     }
 
     init {
