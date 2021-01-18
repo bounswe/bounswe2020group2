e895f90ee37173c31e36ac6ae82b18113cebc54f
karalleyna <36455180+karalleyna@users.noreply.github.com>
2021-01-18 19:48:34 +0300

    Sign up to the user Firebase an send an email to verify

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/RegisterFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/RegisterFragment.kt
@@ -1,11 +1,13 @@
 package com.example.getflix.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -14,6 +16,7 @@ import com.example.getflix.R
 import com.example.getflix.databinding.FragmentRegisterBinding
 import com.example.getflix.doneAlert
 import com.example.getflix.infoAlert
+import com.example.getflix.ui.fragments.RegisterFragmentDirections.Companion.actionRegisterFragmentToMailVerificationFragment
 import com.example.getflix.ui.viewmodels.RegisterViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -159,14 +162,31 @@ class RegisterFragment : Fragment() {
             if (it != null && it.message == "Username is already in use") {
                 infoAlert(this, getString(R.string.user_already_exists))
                 navigateLogin()
+                println("Username is already in use login fragment")
             } else if (it != null && it.successful) {
-                println("oturum acıyor")
                 // doneAlert()
-
+                println("Login olabildi, register fragmennt")
+                if(registerViewModel.emailVerificationSent.value!!){
+                    registerViewModel.onEmailVerificationMailComplete()
+                    view?.findNavController()?.navigate(actionRegisterFragmentToMailVerificationFragment())
+                }
             } else {
-                println("nasil yani")
+                println("Login olamadı")
             }
         })
+        registerViewModel.emailVerificationSent.observe(viewLifecycleOwner, Observer {
+            if (it != null) {
+                if (it) {
+                    Toast.makeText(context,"Please, verify the mail we have just sent!",Toast.LENGTH_SHORT)
+                    println("RegisterFragment:Verification olması için registerda mail yollandı")
+                } else {
+                    Toast.makeText(context,"Oops! Something went wrong... Please, try again!",Toast.LENGTH_SHORT)
+                    println("RegisterFragment:Verification olması için registerda mail yollanmaya çalıştı ama olmadı")
+                }
+                registerViewModel.onEmailVerificationMailComplete()
+            }
+        }
+        )
 
         binding.btnBack.setOnClickListener {
             view?.findNavController()?.popBackStack()
--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/viewmodels/RegisterViewModel.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/viewmodels/RegisterViewModel.kt
@@ -1,31 +1,28 @@
 package com.example.getflix.ui.viewmodels
 
 import android.app.Application
-import android.util.Log
-import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.example.getflix.activities.MainActivity
 import com.example.getflix.activities.MainActivity.StaticData.auth
 import com.example.getflix.service.requests.SignUpRequest
 import com.example.getflix.service.responses.SignUpResponse
 import com.example.getflix.service.GetflixApi
-import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class RegisterViewModel(application: Application) : AndroidViewModel(application) {
     private val _signUpCredentials = MutableLiveData<SignUpRequest>()
-    val signUpRequest: LiveData<SignUpRequest?>
-        get() = _signUpCredentials
 
     private val _canSignUp = MutableLiveData<SignUpResponse?>()
     val canSignUp: LiveData<SignUpResponse?>
         get() = _canSignUp
+
+    private val _emailVerificationSent = MutableLiveData<Boolean>()
+    val emailVerificationSent: LiveData<Boolean>
+        get() = _emailVerificationSent
 
     fun setSignUpCredentials(
         fragment: Fragment,
@@ -35,7 +32,7 @@ class RegisterViewModel(application: Application) : AndroidViewModel(application
         firstName: String,
         lastName: String,
         phoneNumber: String,
-        conPassword: String
+        conPassword: String,
     ): Boolean {
 
         _signUpCredentials.value =
@@ -43,44 +40,73 @@ class RegisterViewModel(application: Application) : AndroidViewModel(application
         signUp()
         return true
     }
-    init{
+
+    init {
+        _emailVerificationSent.value = false
         _canSignUp.value = null
     }
+
     private fun signUp() {
 
-                    // Sign in success, update UI with the signed-in user's information
-                    GetflixApi.getflixApiService.signUp(_signUpCredentials.value!!)
-                        .enqueue(object :
-                            Callback<SignUpResponse> {
-                            override fun onFailure(call: Call<SignUpResponse>, t: Throwable) {
+        // Sign in success, update UI with the signed-in user's information
+        GetflixApi.getflixApiService.signUp(_signUpCredentials.value!!)
+            .enqueue(object :
+                Callback<SignUpResponse> {
+                override fun onFailure(call: Call<SignUpResponse>, t: Throwable) {
+                    println("backend endpointi çalışmıyor.")
+                    _canSignUp.value = null
+                }
+
+                override fun onResponse(
+                    call: Call<SignUpResponse>,
+                    response: Response<SignUpResponse>
+                ) {
+                    _canSignUp.value = response.body()
+                    println("Şu an backend endpointi doğru girdi.")
+                    println(response.body())
+                    if (response.body() != null && response.body()!!.successful) {
+                        auth.createUserWithEmailAndPassword(
+                            _signUpCredentials.value!!.email,
+                            _signUpCredentials.value!!.password
+                        )
+                            .addOnCompleteListener {
+                                println("Şu an fire base register'a girdi")
+                                val firebaseUser = auth.currentUser
+                                if (firebaseUser?.isEmailVerified == false) {
+                                    firebaseUser.let {
+                                        it.sendEmailVerification().addOnSuccessListener {
+                                            println("Şu an fire base register maili attı")
+                                            _emailVerificationSent.value = true
+                                        }.addOnFailureListener {
+                                            println("Şu an firebase register maili atamadı")
+                                            _emailVerificationSent.value = false
+                                        }
+                                    }
+                                }
+
+                                if (it.isSuccessful || response.body()?.message == "Username is already in use") {
+                                    println("Aynı  kullanıcı var veya başarılı kısmına girdi on responseda")
+
+                                } else {
+                                    println("Hata kısmındayız şu an on responnseun hata:" + response.body()?.message)
+                                    _canSignUp.value = null
+                                }
+                            }.addOnFailureListener {
+                                println("Fire base mail atamama hatası :" + it.message)
                                 _canSignUp.value = null
                             }
 
-                            override fun onResponse(
-                                call: Call<SignUpResponse>,
-                                response: Response<SignUpResponse>
-                            ) {
-                                _canSignUp.value = response.body()
-                                println(response.message())
-                                auth.createUserWithEmailAndPassword(
-                                    _signUpCredentials.value!!.email,
-                                    _signUpCredentials.value!!.password
-                                )
-                                    .addOnCompleteListener {
-                                        if (it.isSuccessful || response.body()?.message == "Username is already in use" ) {
-                                            println("Çalışma")
-                                        }
-                                        else {
-                                            println("Hata1" + response.body()?.message)
-                                            _canSignUp.value = null
-                                        }
-                                    }
 
-                            }
-                        }
+                    }
+                }
+            })
+    }
 
-                        )
+
+    fun onEmailVerificationMailComplete() {
+        _emailVerificationSent.value = null
     }
 }
+
 
 
