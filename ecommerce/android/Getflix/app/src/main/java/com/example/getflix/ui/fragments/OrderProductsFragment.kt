e7d25a2f3ba4cfe7811c0b9f55da6822599bbf07
hande <hansirikci@gmail.com>
2021-01-19 23:23:20 +0300

    mock data added for test

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/OrderProductsFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/OrderProductsFragment.kt
@@ -19,8 +19,37 @@ class OrderProductsFragment : Fragment() {
             container, false
         )
         activity?.toolbar!!.toolbar_title.text = getString(R.string.order_products)
+
         viewModel = ViewModelProvider(this).get(OrderProductViewModel::class.java)
         val recView = binding?.listProductList as RecyclerView
+
+        val list1 = listOf<String>()
+        val list2 = mutableListOf<SubcategoryModel>()
+        var product= ProductModel(27, "Samsung S20 Ultra", 10999.0, "2020-12-26T10:47:38.961041Z", 50, 11, 50,
+            "Ekran Boyutu: 6.2', Ekran Çözünürlüğü: 1440x3200 px, Arka Kamera: 12 MP, Üçlü Kamera, Ön Kamera: 10 MP, 4G, Dahili Hafıza: 128 GB",
+            SubcategoryModel("Cell Phones & Accessories", 2),
+            "Galaxy S serisi akıllı cep telefonlarıyla nefes kesici teknolojik yenilikleri sergileyen Samsung, sinematik kare/saniye oranlarında ve 8K çözünürlükte video kaydı yapan",
+            0.045 , CategoryModel("Electronics", 1, list2), BrandModel("Samsung",18), VendorModel(0.0,3, "Can Batuk"),
+            4.545454545454546,
+            list1, 10504.045, false)
+        var purchasedModel = OrderPurchasedModel(1, 1, product, "",1,"",VendorModel(0.0,3, "Can Batuk"),)
+        val listproducts = arrayListOf(purchasedModel)
+        val listAdapter = OrderProductsAdapter(listproducts)
+        recView.adapter = listAdapter
+        recView.setHasFixedSize(true)
+
+        for (product in listproducts) {
+            viewModel.addList(product)
+        }
+
+        viewModel.purchasedProductList.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                listAdapter.submitList(it)
+            }
+        })
+
+
+        return binding.root
     }
 
 }
