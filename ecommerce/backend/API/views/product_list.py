6a21380b74c045a0512048a3276f90e66cf65197
bekir <bekir1996mersin@gmail.com>
2021-01-20 01:55:34 +0300

    product list get method implemented and remove unused field(name) from product table, also migration is satisfied

--- ecommerce/backend/API/views/product_list.py
+++ ecommerce/backend/API/views/product_list.py
@@ -8,7 +8,7 @@ from rest_framework.parsers import JSONParser
 
 from ..utils import permissions, Role
 from ..models import User, ShoppingCartItem, Product, Address, Order, Purchase, Card, ProductList, ProductListItem
-from ..serializers import checkout_product_serializer, checkout_shopping_cart_serializer, address_serializer, shopping_cart_serializer
+from ..serializers import product_serializer
 from ..utils import authentication, order_status
 
 
@@ -17,11 +17,27 @@ from ..utils import authentication, order_status
 def product_list_create(request):
     jwt = authentication.JWTAuthentication()
     user = jwt.authenticate(request=request)[0]
-
-    list_name = request.data.get("name")
-
-    product_list = ProductList(user_id=user.pk, name=list_name)
-    product_list.save()
-
-    return Response({'id': product_list.pk, 'status': { 'successful': True, 'message': "List is successfully created."}})
     
+    if request.method == "POST":
+        list_name = request.data.get("name")
+
+        product_list = ProductList(user_id=user.pk, name=list_name)
+        product_list.save()
+
+        return Response({'id': product_list.pk, 'status': { 'successful': True, 'message': "List is successfully created."}})
+
+    elif request.method == "GET":
+        response_product_list = []
+        product_lists = ProductList.objects.filter(user_id=user.pk, many=True)
+
+        for list in product_lists:
+            list_item = ProductListItem.objects.filter(product_list_id=list.pk, many=True)
+            products_serializer = product_serializer.ProductResponseSerializer(list_item, many=True)
+
+            product_lists.append({
+                                'list_id':list.pk, 
+                                'name':list.name,
+                                'products': products_serializer.data 
+                                })
+
+        return Response({'status': {'successful': True, 'message':'Product Lists are uccessfully sent'},'lists':product_lists})
