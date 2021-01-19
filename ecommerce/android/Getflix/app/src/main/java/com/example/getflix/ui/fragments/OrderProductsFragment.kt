2f4d06cd188c08404cf218c6aeeaa2a4b5e8b0ec
hande <hansirikci@gmail.com>
2021-01-19 23:23:35 +0300

    libraries implemented

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/OrderProductsFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/OrderProductsFragment.kt
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.getflix.R
+import com.example.getflix.databinding.FragmentOrderProductsBinding
+import com.example.getflix.models.*
+import com.example.getflix.ui.adapters.OrderProductsAdapter
+import com.example.getflix.ui.viewmodels.OrderProductViewModel
 
 
 class OrderProductsFragment : Fragment() {
+
+    private lateinit var viewModel: OrderProductViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
