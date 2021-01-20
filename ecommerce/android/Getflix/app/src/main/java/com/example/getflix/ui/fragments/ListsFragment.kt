6cf49fddd2f98a787ca78aeff2cd5a59d8cc2ffb
Asli Aykan <asliayk@hotmail.com>
2021-01-21 00:12:40 +0300

    click listener for fab button added

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/ListsFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/ListsFragment.kt
@@ -27,12 +27,12 @@ class ListsFragment : Fragment() {
     private lateinit var viewModel: ListViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentListsBinding>(
-                inflater, R.layout.fragment_lists,
-                container, false
+            inflater, R.layout.fragment_lists,
+            container, false
         )
 
         viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
@@ -53,13 +53,26 @@ class ListsFragment : Fragment() {
                 ProductModel(6, "Skirt", "102", "1", "Zara", 1, 1, 1, "Amazing skirt", "1", "1", "1", "1")*/
         val list1 = listOf<String>()
         val list2 = mutableListOf<SubcategoryModel>()
-        var product= ProductModel(27, "Samsung S20 Ultra", 10999.0, "2020-12-26T10:47:38.961041Z", 50, 11, 50,
-        "Ekran Boyutu: 6.2', Ekran Çözünürlüğü: 1440x3200 px, Arka Kamera: 12 MP, Üçlü Kamera, Ön Kamera: 10 MP, 4G, Dahili Hafıza: 128 GB",
-        SubcategoryModel("Cell Phones & Accessories", 2),
-        "Galaxy S serisi akıllı cep telefonlarıyla nefes kesici teknolojik yenilikleri sergileyen Samsung, sinematik kare/saniye oranlarında ve 8K çözünürlükte video kaydı yapan",
-            0.045 , CategoryModel("Electronics", 1, list2), BrandModel("Samsung",18), VendorModel(0.0,3, "Can Batuk"),
+        var product = ProductModel(
+            27,
+            "Samsung S20 Ultra",
+            10999.0,
+            "2020-12-26T10:47:38.961041Z",
+            50,
+            11,
+            50,
+            "Ekran Boyutu: 6.2', Ekran Çözünürlüğü: 1440x3200 px, Arka Kamera: 12 MP, Üçlü Kamera, Ön Kamera: 10 MP, 4G, Dahili Hafıza: 128 GB",
+            SubcategoryModel("Cell Phones & Accessories", 2),
+            "Galaxy S serisi akıllı cep telefonlarıyla nefes kesici teknolojik yenilikleri sergileyen Samsung, sinematik kare/saniye oranlarında ve 8K çözünürlükte video kaydı yapan",
+            0.045,
+            CategoryModel("Electronics", 1, list2),
+            BrandModel("Samsung", 18),
+            VendorModel(0.0, 3, "Can Batuk"),
             4.545454545454546,
-            list1, 10504.045, false)
+            list1,
+            10504.045,
+            false
+        )
         val products = arrayListOf(product)
         var list3 =
             ListModel(10, "My Summer Collection", products)
@@ -77,24 +90,44 @@ class ListsFragment : Fragment() {
         viewModel.listList.observe(viewLifecycleOwner, Observer {
             it?.let {
                 var size = 0
-                if(size==0) {
+                if (size == 0) {
                     binding.btnAddList.visibility = View.VISIBLE
                     binding.listImage.visibility = View.VISIBLE
                     binding.listText.visibility = View.VISIBLE
                     binding.listsList.visibility = View.GONE
+                    binding.addFab.visibility = View.GONE
                 } else {
                     binding.btnAddList.visibility = View.GONE
                     binding.listImage.visibility = View.GONE
                     binding.listText.visibility = View.GONE
                     binding.listsList.visibility = View.VISIBLE
+                    binding.addFab.visibility = View.VISIBLE
                     listAdapter.submitList(it)
                 }
 
             }
         })
 
-            binding.btnAddList.setOnClickListener {
-                if(binding.btnAddList.visibility == View.VISIBLE) {
+        binding.btnAddList.setOnClickListener {
+            if (binding.btnAddList.visibility == View.VISIBLE) {
+                var dialog = AlertDialog.Builder(context, R.style.MaterialAlertDialog_color)
+                var dialogView = layoutInflater.inflate(R.layout.custom_dialog, null)
+                var edit = dialogView.findViewById<TextInputEditText>(R.id.name)
+                dialog.setView(dialogView)
+                dialog.setCancelable(true)
+                dialog.setIcon(R.drawable.ic_pencil)
+                dialog.setTitle("Add A List")
+                dialog.setNegativeButton("Cancel") { dialogInterface: DialogInterface, i: Int -> }
+                dialog.setPositiveButton("Create") { dialogInterface: DialogInterface, i: Int ->
+                    println(edit.text.toString())
+                    hideKeyboard(requireActivity())
+                }
+                dialog.show()
+            }
+        }
+
+        binding.addFab.setOnClickListener {
+            if (binding.addFab.visibility == View.VISIBLE) {
                 var dialog = AlertDialog.Builder(context, R.style.MaterialAlertDialog_color)
                 var dialogView = layoutInflater.inflate(R.layout.custom_dialog, null)
                 var edit = dialogView.findViewById<TextInputEditText>(R.id.name)
