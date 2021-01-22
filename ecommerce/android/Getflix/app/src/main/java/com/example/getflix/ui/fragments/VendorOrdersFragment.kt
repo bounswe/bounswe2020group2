c5d69734f5e803844082b5f7941dffdf036e3f79
karalleyna <36455180+karalleyna@users.noreply.github.com>
2021-01-22 17:51:21 +0300

    Add navigation to the single page of the. selected order

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/VendorOrdersFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/VendorOrdersFragment.kt
@@ -8,12 +8,15 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.getflix.R
 import com.example.getflix.databinding.FragmentVendorOrdersBinding
 import com.example.getflix.ui.adapters.VendorOrderAdapter
 import com.example.getflix.ui.viewmodels.VendorOrdersViewModel
+import com.example.getflix.vendorOrderModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
@@ -46,6 +49,8 @@ class VendorOrdersFragment : Fragment() {
 
         vendorOrderAdapter.selectedStatus.observe(viewLifecycleOwner, Observer {
             if (it != null) {
+                println(it)
+                println(vendorOrderAdapter.selectedOrder.value.toString())
                 vendorOrdersViewModel.changeStatusOfOrder(
                     vendorOrderAdapter.selectedOrder.value!!,
                     it,
@@ -54,6 +59,19 @@ class VendorOrdersFragment : Fragment() {
                 vendorOrderAdapter.onSelectionCompleted()
             }
         })
+
+        vendorOrdersViewModel.orders.observe(viewLifecycleOwner, Observer {
+            vendorOrderAdapter.submitList(it)
+        })
+        vendorOrderAdapter.destination.observe(viewLifecycleOwner, Observer {
+            if(it!=null) {
+                vendorOrderModel = it!!
+                findNavController().navigate(
+                    VendorOrdersFragmentDirections.actionVendorOrdersFragmentToVendorOrderFragment(),
+                    vendorOrderAdapter.fragmentNavigatorExtras!!
+                )
+            }
+            })
 
         return binding.root
     }
