e61cdf296e0187ec8e045e0b07d2d9a402540cbc
Ömer Faruk Deniz <omerfaruk.deniz@gmail.com>
2021-01-22 16:11:30 +0300

    added serializer for messages in the response

--- ecommerce/backend/API/serializers/message_serializer.py
+++ ecommerce/backend/API/serializers/message_serializer.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
+from ..models import Message
 
 # Formats the body of the POST request to make it compatible with the Message model in the database
 class MessageRequestSerializer(serializers.Serializer):
     receiver_id = serializers.IntegerField()
     text = serializers.CharField()
     attachment = serializers.CharField(allow_null = True)
+
+# Formats the Message objects taken from the database for GET requests
+class MessageResponseSerializer(serializers.ModelSerializer):
+    sent_by_me = serializers.SerializerMethodField('get_sent_by_me')
+    class Meta:
+        model = Message
+        fields = ('id', 'text', 'sent_by_me', 'date', 'attachment_url')
+    
+    def get_sent_by_me(self, obj):
+        return obj.sender == self.context["sender"]
