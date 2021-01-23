84610d31d80343d06ee50631bc1b98f2ef7bad1e
karalleyna <36455180+karalleyna@users.noreply.github.com>
2021-01-24 00:50:37 +0300

    Create NByThreeViewModel.kt

---
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/viewmodels/NByThreeViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.getflix.ui.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.getflix.models.ProductModel
+import com.example.getflix.models.VendorModel
+import com.example.getflix.service.GetflixApi
+import com.example.getflix.service.responses.ProSearchByVendorResponse
+import com.example.getflix.vendorModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class NByThreeViewModel: ViewModel() {
+
+
+    private val _products = MutableLiveData<List<ProductModel>?>()
+    val products: LiveData<List<ProductModel>?>
+        get() = _products
+
+    fun getProductsOfVendor() {
+        GetflixApi.getflixApiService.searchProductsByVendor(vendorModel!!.id)
+            .enqueue(object :
+                Callback<ProSearchByVendorResponse> {
+                override fun onFailure(call: Call<ProSearchByVendorResponse>, t: Throwable) {
+                    _products.value = null
+                }
+
+                override fun onResponse(
+                    call: Call<ProSearchByVendorResponse>,
+                    response: Response<ProSearchByVendorResponse>
+                ) {
+                    if (response.body() != null) {
+                        _products.value = response.body()!!.data.products
+                    } else {
+                        _products.value = null
+                    }
+
+                }
+            }
+            )
+
+    }
+}
