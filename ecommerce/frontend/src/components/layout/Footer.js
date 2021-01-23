32a8c3d9ca4223e52980c99c682569945a2cc9f1
Mehdi Saffar <saffar.mehdi3@gmail.com>
2021-01-23 21:38:16 +0300

    better footer

--- ecommerce/frontend/src/components/layout/Footer.js
+++ ecommerce/frontend/src/components/layout/Footer.js
@@ -1,24 +1,55 @@
 import './Layout_common.less'
 import { Layout } from 'antd'
+import MobileStoreButton from 'react-mobile-store-button'
+
+import storeButtons from '../../assets/both.png'
+
+import { FacebookOutlined, InstagramOutlined, TwitterOutlined } from '@ant-design/icons'
 
 export const Footer = () => {
     const cols = [
         [['About us'], ['My Account'], ['Orders History']],
-        [['Contact us'], ['Address'], ['Email'], ['Working Days']],
+        [['Contact us'], ['Address'], ['Email support@getflix.com'], ['']],
         [['Follow Us'], ['Facebook'], ['Twitter'], ['Instagram']],
     ]
 
-    const renderCol = (col, ix1) => {
-        const renderEntry = (entry, ix2) => {
-            return <p key={ix2}>{entry}</p>
-        }
-
-        return (
-            <div key={ix1} className="footer-col">
-                {col.map(renderEntry)}
+    return (
+        <Layout.Footer className="footer">
+            <div className="footer-col">
+                <div className="footer-row-main">About us</div>
+                <div>Career</div>
+                <div>Support center</div>
+                <div>Site roadmap</div>
+                <div>Secure shopping guide</div>
             </div>
-        )
-    }
-
-    return <Layout.Footer className="footer">{cols.map(renderCol)}</Layout.Footer>
+            <div className="footer-col">
+                <div className="footer-row-main">Follow us on:</div>
+                <div>
+                    <FacebookOutlined />
+                    &nbsp; Facebook
+                </div>
+                <div>
+                    <InstagramOutlined />
+                    &nbsp; Instagram
+                </div>
+                <div>
+                    <TwitterOutlined />
+                    &nbsp; Twitter
+                </div>
+            </div>
+            <div className="footer-col">
+                <div className="footer-row-main">Do you have a question or feedback for us?</div>
+                <div>Call our support desk at </div>
+                <div>&nbsp;&nbsp;+90 2430 3242 23 34</div>
+                <div>Send us an email at</div>
+                <div>&nbsp;&nbsp;support@getflix.com</div>
+            </div>
+            <div className="footer-col">
+                <div className="footer-row-main">For a better experience</div>
+                <div>
+                    <img src={storeButtons} style={{ height: 120, background: 'white', borderRadius: 8, padding: 4 }} />
+                </div>
+            </div>
+        </Layout.Footer>
+    )
 }
