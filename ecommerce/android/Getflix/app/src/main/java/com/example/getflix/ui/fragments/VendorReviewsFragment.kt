85b885b85b993909f8748113b1f7442fe51516fd
karalleyna <36455180+karalleyna@users.noreply.github.com>
2021-01-24 04:48:32 +0300

    Implement VendorReviewsFragment

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/VendorReviewsFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/VendorReviewsFragment.kt
@@ -5,24 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.getflix.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.getflix.databinding.FragmentVendorReviewsBinding
+import com.example.getflix.ui.adapters.VendorCommentAdapter
+import com.example.getflix.ui.viewmodels.VendorReviewsViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class VendorReviewsFragment : Fragment() {
-
-
+    private lateinit var binding: FragmentVendorReviewsBinding
+    private lateinit var vendorReviewsViewModel: VendorReviewsViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vendor_reviews, container, false)
-    }
+        activity?.toolbar_lay!!.visibility = View.GONE
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_vendor_reviews,
+            container, false
+        )
+        binding.lifecycleOwner = this
+        vendorReviewsViewModel = ViewModelProvider(this).get(VendorReviewsViewModel::class.java)
+        vendorReviewsViewModel.products.observe(viewLifecycleOwner, Observer {
+            if (it != null) {
+                vendorReviewsViewModel.getAllReviewsOfVendor()
+            }
+        })
 
+        val adapter = VendorCommentAdapter()
+        binding.reviews.adapter = adapter
+        val layoutManager = LinearLayoutManager(activity)
+        binding.reviews.layoutManager = layoutManager
+        vendorReviewsViewModel.reviews.observe(viewLifecycleOwner, Observer {
+            if (it != null) {
+                println(it)
+                adapter.submitList(it)
+            }
+        })
+        return binding.root
+    }
 
 }
