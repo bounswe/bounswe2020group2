7e404705821b9ecd325512946e581940d3c43c23
Asli Aykan <asliayk@hotmail.com>
2021-01-26 09:24:23 +0300

    secret key handled

--- ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/VendorOrderFragment.kt
+++ ecommerce/android/Getflix/app/src/main/java/com/example/getflix/ui/fragments/VendorOrderFragment.kt
@@ -34,12 +34,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 
-class VendorOrderFragment : Fragment() {
-    //, OnMapReadyCallback {
+class VendorOrderFragment : Fragment()
+    , OnMapReadyCallback {
     private lateinit var binding: FragmentVendorOrderBinding
     private lateinit var vendorOrderViewModel: VendorOrderViewModel
     private lateinit var currentOrder: VendorOrderModel
-    //private lateinit var fragmentGoogleMap: GoogleMap
+    private lateinit var fragmentGoogleMap: GoogleMap
 
 
     override fun onCreateView(
@@ -182,12 +182,12 @@ class VendorOrderFragment : Fragment() {
         binding.orderOwnerZipCode.text = currentOrder.address.zipCode
         binding.phone.text = currentOrder.address.phone.countryCode + " " + currentOrder.address.phone.number
 
-      //  binding.mapView.onCreate(savedInstanceState)
-      //  binding.mapView.getMapAsync(this)
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync(this)
 
         return binding.root
     }
-   /* override fun onResume() {
+    override fun onResume() {
         super.onResume()
         binding.mapView.onResume()
     }
@@ -233,6 +233,6 @@ class VendorOrderFragment : Fragment() {
         }
 
 
-    }   */
+    }
 
 }
